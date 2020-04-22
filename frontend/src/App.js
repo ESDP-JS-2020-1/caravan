9811f008beb766f3488fe73693b7175fb880fd53
Kostya Fedchenko <kostya.fedchenko.00@mail.ru>
2020-04-22 19:53:20 +0600

    добавил "<Route path="/login" exact component={Login}/>"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import {Route, Switch} from "react-router-dom";
+import Login from "./containers/Login/Login";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+      <>
+        <header>
+          {/*место для header*/}
+        </header>
+        <Switch>
+          <Route path="/login" exact component={Login}/>
+        </Switch>
+      </>
   );
 }
 
