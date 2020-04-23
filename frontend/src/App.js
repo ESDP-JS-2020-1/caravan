138e9b03735376a5225717b541776e3aa8d661ea
spartak <fomenkospartak@gmail.com>
2020-04-23 12:18:03 +0600

    add index.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import {Route, Switch} from "react-router-dom";
 import Login from "./containers/Login/Login";
 import Navigation from "./components/Navigation/Navigation";
-
+import "./index.css"
 function App() {
   return (
       <>
