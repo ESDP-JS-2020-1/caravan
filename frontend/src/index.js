453ba5905d44f631722509675efa49af09735285
Ulan Beltaev <ulanbeltaev@gmail.com>
2020-04-23 16:15:40 +0600

    Добавил файл css react-toastify

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {ConnectedRouter} from "connected-react-router";
+import 'react-toastify/dist/ReactToastify.css';
 import * as serviceWorker from './serviceWorker';
 import store, {history} from "./store/configureStore";
 import App from './App';
