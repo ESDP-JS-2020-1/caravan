36149f2e81b643902e22572589e10abba75c4ba5
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-04-23 21:18:27 +0600

    Добавил кнопку, ведущую на форму добавления нового пользователя

--- frontend/src/components/UsersList/UsersList.js
+++ frontend/src/components/UsersList/UsersList.js
@@ -2,6 +2,10 @@ import React, {useEffect} from 'react';
 import MaterialTable from 'material-table';
 import {useDispatch, useSelector} from "react-redux";
 import {getUsers} from "../../store/actions/usersActions";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
+import {NavLink} from "react-router-dom";
 
 export default function MaterialTableDemo(props) {
 
@@ -12,25 +16,42 @@ export default function MaterialTableDemo(props) {
     const users = useSelector(state => state.users.users)
 
     return (
-        <MaterialTable
-            style={{marginTop: '5%'}}
-            title="Список пользователей"
-            columns={[
-                {title: 'Display Name', field: 'displayName'},
-                {title: 'Username', field: 'username'},
-                {title: 'Role', field: 'role'},
-            ]}
-            data={users ? users : []}
-            actions={[
-                {
-                    icon: 'edit',
-                    tooltip: 'save user',
-                    onClick: () => {
-                        props.history.push('/edit')
-                    }
-                }
-            ]}
-        />
+        <Box mt={2}>
+            <Grid container justify='flex-end'>
+                <Grid item>
+                    <Button
+                        variant='contained'
+                        color='primary'
+                        component={NavLink}
+                        to='/users/new'
+                    >Добавить нового пользователя</Button>
+                </Grid>
+            </Grid>
+            <Box mt={2}>
+                <Grid container>
+                    <Grid item xs>
+                        <MaterialTable
+                            title="Список пользователей"
+                            columns={[
+                                {title: 'Display Name', field: 'displayName'},
+                                {title: 'Username', field: 'username'},
+                                {title: 'Role', field: 'role'},
+                            ]}
+                            data={users ? users : []}
+                            actions={[
+                                {
+                                    icon: 'edit',
+                                    tooltip: 'save user',
+                                    onClick: () => {
+                                        props.history.push('/edit')
+                                    }
+                                }
+                            ]}
+                        />
+                    </Grid>
+                </Grid>
+            </Box>
+        </Box>
     );
 }
 
