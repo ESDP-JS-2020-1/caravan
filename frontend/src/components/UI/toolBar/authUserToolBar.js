2b3a29eebc11bcb8db3e42e57929424d4d41adfa
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-04-23 14:03:13 +0600

    Добавил кнопку "список пользователей" для пользователя администратора

--- frontend/src/components/UI/toolBar/authUserToolBar.js
+++ frontend/src/components/UI/toolBar/authUserToolBar.js
@@ -7,6 +7,8 @@ import MenuItem from "@material-ui/core/MenuItem";
 import ListItem from "@material-ui/core/ListItem";
 import Divider from "@material-ui/core/Divider";
 import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import {NavLink} from "react-router-dom";
 
 const AuthUserToolBar = () => {
 
@@ -32,6 +34,11 @@ const AuthUserToolBar = () => {
 
     return (
         <>
+            {user.role === 'admin' && (
+                <Button variant='contained' component={NavLink} to='/users'>
+                    Список пользователей
+                </Button>
+            )}
             <IconButton
                 aria-label="account of current user"
                 aria-controls="menu-appbar"
