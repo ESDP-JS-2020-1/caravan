ff58cca79a0dcec89c86a4a433444dcf3c721b08
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-04-23 14:08:41 +0600

    Поместил Switch в Container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,18 @@ import {Route, Switch} from "react-router-dom";
 import Login from "./containers/Login/Login";
 import Navigation from "./components/Navigation/Navigation";
 import "./index.css"
+import UsersList from "./components/UsersList/UsersList";
+import Container from "@material-ui/core/Container";
 function App() {
   return (
       <>
         <Navigation/>
-        <Switch>
-          <Route path="/login" exact component={Login}/>
-        </Switch>
+        <Container>
+            <Switch>
+                <Route path="/login" exact component={Login}/>
+                <Route path="/users" exact component={UsersList}/>
+            </Switch>
+        </Container>
       </>
   );
 }
