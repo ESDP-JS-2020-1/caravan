fb54c06204de1646dd78fa11ba54c1f750531665
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-04-23 14:57:13 +0600

    Добавил интерсептор для добавления заголовков запроса

--- frontend/src/axiosAPI.js
+++ frontend/src/axiosAPI.js
@@ -1,8 +1,18 @@
 import axios from 'axios';
 import {apiURL} from "./apiURL";
+import {store} from "./store/configureStore";
 
 const axiosApi = axios.create({
     baseURL: apiURL
 });
+
+axiosApi.interceptors.request.use(config => {
+    if(store.getState().users.user) {
+        const token = store.getState().users.user.token;
+
+        config.headers.Authorization = 'token ' + token;
+    }
+    return config;
+})
 
 export default axiosApi;
