b5f217af1d96336af49a978ec25d8be534a3d6a2
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-04-23 12:55:01 +0600

    Изменил user на currentUser

--- backend/middleware/permit.js
+++ backend/middleware/permit.js
@@ -1,7 +1,7 @@
 const permit = (...roles) => (req, res, next) => {
     if(!req.currentUser) return res.status(401).send({error: 'User is unauthenticated'});
 
-    if(!roles.includes(req.user.role)) return res.status(403).send({error: 'User is unauthenticated'});
+    if(!roles.includes(req.currentUser.role)) return res.status(403).send({error: 'User is unauthenticated'});
 
     next();
 };
