e362c4ce0c2c49403be255786540c7067e399c64
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-04-23 22:57:32 +0600

    Изменил отображение списка пользователей

--- frontend/src/components/UsersList/UsersList.js
+++ frontend/src/components/UsersList/UsersList.js
@@ -1,57 +1,78 @@
 import React, {useEffect} from 'react';
-import MaterialTable from 'material-table';
 import {useDispatch, useSelector} from "react-redux";
 import {getUsers} from "../../store/actions/usersActions";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import Box from "@material-ui/core/Box";
 import {NavLink} from "react-router-dom";
+import TableContainer from "@material-ui/core/TableContainer";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import TableBody from "@material-ui/core/TableBody";
+import UserListItem from "./UserListItem/UserListItem";
 
-export default function MaterialTableDemo(props) {
+const UserList = () => {
 
-    const dispatch = useDispatch()
-    useEffect(() => {
-        dispatch(getUsers())
-    }, [dispatch])
-    const users = useSelector(state => state.users.users)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(getUsers())
+  }, [dispatch])
 
-    return (
-        <Box mt={2}>
-            <Grid container justify='flex-end'>
-                <Grid item>
-                    <Button
-                        variant='contained'
-                        color='primary'
-                        component={NavLink}
-                        to='/users/new'
-                    >Добавить нового пользователя</Button>
-                </Grid>
-            </Grid>
-            <Box mt={2}>
-                <Grid container>
-                    <Grid item xs>
-                        <MaterialTable
-                            title="Список пользователей"
-                            columns={[
-                                {title: 'Display Name', field: 'displayName'},
-                                {title: 'Username', field: 'username'},
-                                {title: 'Role', field: 'role'},
-                            ]}
-                            data={users ? users : []}
-                            actions={[
-                                {
-                                    icon: 'edit',
-                                    tooltip: 'save user',
-                                    onClick: () => {
-                                        props.history.push('/edit')
-                                    }
-                                }
-                            ]}
-                        />
-                    </Grid>
-                </Grid>
-            </Box>
-        </Box>
-    );
+  const users = useSelector(state => state.users.users)
+
+  return (
+    <Box mt={2}>
+      <Grid container justify='flex-end'>
+        <Grid item>
+          <Button
+            variant='contained'
+            color='primary'
+            component={NavLink}
+            to='/users/new'
+          >Добавить нового пользователя</Button>
+        </Grid>
+      </Grid>
+      <Box mt={2}>
+        <Grid container>
+          <Grid item xs>
+            <TableContainer component={Paper}>
+              <Table aria-label="caption table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell><b>имя</b></TableCell>
+                    <TableCell><b>логин</b></TableCell>
+                    <TableCell><b>телефон</b></TableCell>
+                    <TableCell><b>название компании</b></TableCell>
+                    <TableCell><b>адрес</b></TableCell>
+                    <TableCell><b>роль</b></TableCell>
+                    <TableCell> </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {users && users.map(user => (
+                    <UserListItem
+                      key={user._id}
+                      id={user._id}
+                      displayName={user.displayName}
+                      username={user.username}
+                      phone={user.phone}
+                      companyName={user.companyName}
+                      address={user.address}
+                      role={user.role}
+                    />
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  );
 }
+
+export default UserList;
 
