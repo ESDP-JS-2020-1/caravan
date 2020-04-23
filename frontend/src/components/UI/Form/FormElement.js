86f4c5443c19b27ffd7adc94fc3dd1410ceae528
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-04-23 19:28:15 +0600

    Добавил тип select в FormElement.js

--- frontend/src/components/UI/Form/FormElement.js
+++ frontend/src/components/UI/Form/FormElement.js
@@ -2,30 +2,84 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {makeStyles} from '@material-ui/core/styles';
 import TextField from "@material-ui/core/TextField";
+import PhotoCameraIcon from '@material-ui/icons/PhotoCamera';
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 
 const useStyles = makeStyles(() => ({
-  root: {
-    width: '100%',
-    marginBottom: '2%',
-  },
+    root: {
+        width: '100%',
+        marginBottom: '2%',
+    },
+    input: {
+        display: 'none',
+    },
 }));
 const FormElement = props => {
-  const classes = useStyles();
-    return (
-        <TextField
-            className={classes.root}
-            label={props.title}
-            variant="filled"
-            error={!!props.error}
-            type={props.type}
-            name={props.propertyName} id={props.propertyName}
-            value={props.value}
-            onChange={props.onChange}
-            required={props.required}
-            autoComplete={props.autoComplete}
-            placeholder={props.placeholder}
-        />
-    );
+    const classes = useStyles();
+
+    let field = <TextField
+        className={classes.root}
+        label={props.title}
+        variant="outlined"
+        error={!!props.error}
+        type={props.type}
+        name={props.propertyName} id={props.propertyName}
+        value={props.value}
+        onChange={props.onChange}
+        required={props.required}
+        autoComplete={props.autoComplete}
+        placeholder={props.placeholder}
+    />
+
+    if (props.type === 'file') {
+        field = (
+            <>
+                <input
+                    accept="image/*"
+                    className={classes.input}
+                    id="contained-button-file"
+                    multiple
+                    type="file"
+                    name={props.propertyName}
+                    onChange={props.onChange}
+                />
+                <label htmlFor="contained-button-file">
+                    <div>
+                        <Button variant="outlined" color="primary" component="span" startIcon={<PhotoCameraIcon/>}>
+                            {props.title}
+                        </Button>
+                        {props.value && <Typography variant='h6' display='inline'>
+                            {props.value.name}
+                        </Typography>}
+                    </div>
+                </label>
+            </>
+        )
+    }
+
+    if (props.type === 'select') {
+        field = (
+            <>
+                <Select
+                    variant="outlined"
+                    value={props.value}
+                    onChange={props.onChange}
+                    name={props.propertyName}
+                    label={props.title}
+                    fullWidth
+                >
+                    {props.options.map(option => (
+                        <MenuItem value={option} key={option}>{option}</MenuItem>
+                    ))}
+                </Select>
+            </>
+        )
+    }
+
+    return field;
 };
 
 FormElement.propTypes = {
@@ -33,7 +87,6 @@ FormElement.propTypes = {
     title: PropTypes.string.isRequired,
     required: PropTypes.bool,
     placeholder: PropTypes.string,
-    value: PropTypes.string.isRequired,
     onChange: PropTypes.func.isRequired,
     error: PropTypes.string,
     autoComplete: PropTypes.string
