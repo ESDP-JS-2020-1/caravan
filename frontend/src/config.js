f57cf0b95800b3d2838e20be6ab04337a4f40824
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-04-24 23:32:30 +0600

    Добавил config файл

---
+++ frontend/src/config.js
@@ -0,0 +1,13 @@
+export default {
+  notification: {
+    type: "default",
+    insert: "top",
+    container: "top-right",
+    animationIn: ["animated", "fadeIn"],
+    animationOut: ["animated", "fadeOut"],
+    dismiss: {
+      duration: 3000,
+      onScreen: true
+    }
+  }
+}
