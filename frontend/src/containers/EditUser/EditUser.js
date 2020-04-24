18d078602519ee2cd74d2bced131c0fe89c76cd5
ramazan <assasins11122001@gmail.com>
2020-04-24 22:46:17 +0600

    Добавил форму редактирования пользователя

--- frontend/src/containers/EditUser/EditUser.js
+++ frontend/src/containers/EditUser/EditUser.js
@@ -1,182 +1,219 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
+import {deleteUser, editUser, getUser, getUserSuccess} from "../../store/actions/usersActions";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
-import TextField from "@material-ui/core/TextField/TextField";
+import FormElement from "../../components/UI/Form/FormElement";
+import MuiPhoneNumber from "material-ui-phone-number";
+import Alert from "@material-ui/lab/Alert";
 import Button from "@material-ui/core/Button";
-import {deleteUser, editUser, getEditUsers} from "../../store/actions/usersActions";
-import InputLabel from "@material-ui/core/InputLabel";
-import {useParams} from 'react-router-dom';
-import DeleteIcon from '@material-ui/icons/Delete';
+import {Container} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
 import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
-import Alert from "@material-ui/lab/Alert";
+import DeleteIcon from '@material-ui/icons/Delete';
 
-const ROLES = ['market', 'courier', 'admin', 'operator'];
+const useStyles = makeStyles({
+    formBtn: {
+        marginTop: '1%',
+        display: 'block',
+        textAlign: 'center',
+    },
+    formButton: {
+        fontWeight: 'bold',
+        width: '100%',
+        minHeight: '50px'
+    },
+    typography: {
+        color: '#0d47a1',
+        textAlign: 'center',
+    },
+    typographyText: {
+        borderBottom: '2px solid #0d47a1',
+        textTransform: 'uppercase',
+        fontWeight: 'bold',
+        marginBottom: '3%',
+    },
+    phoneInput: {
+        width: '100%',
+    }
+});
 
-const EditUser = () => {
+const EditUser = props => {
+    const classes = useStyles();
 
-  const dispatch = useDispatch();
-  const params = useParams();
+    const dispatch = useDispatch();
 
-  const user = useSelector(state => state.users);
-  const error = useSelector(state => state.users.error)
+    const editClient = useSelector(state => state.users.client);
 
-  const [state, setState] = useState(null);
-  const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
 
+    useEffect(() => {
+        dispatch(getUser(props.match.params.id));
+    }, [dispatch, props.match.params.id]);
 
-  useEffect(() => {
-    dispatch(getEditUsers(params.id));
-    setState({...user})
-  }, []);
+    const error = useSelector(state => state.users.error);
 
-  const editProfile = e => {
-    e.preventDefault()
+    const removeUser = async () => {
+        await dispatch(deleteUser(props.match.params.id))
+    };
 
-    const User = new FormData();
-    Object.keys(state).forEach(item => { User.append(item, state[item]) })
+    const inputChangeHandler = e => dispatch(getUserSuccess({...editClient, [e.target.name]: e.target.value}));
+    const phoneChangeHandler = value => dispatch(getUserSuccess({...editClient, phone: value}));
+    const fileChangeHandler = e => dispatch(getUserSuccess({...editClient, [e.target.name]: e.target.files[0]}));
+    const handleClickOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
-    dispatch(editUser(User));
-  };
+    const roles = ['admin', 'courier', 'operator', 'market'];
 
-  const removeUser = async () => {
-    await dispatch(deleteUser(params.id))
-  }
+    const onSubmit = e => {
+        e.preventDefault();
 
-  const changeInputHandler = e => setState({...state, [e.target.name]: e.target.value})
-  const changeFileHandler = e => setState({...state, [e.target.name]: e.target.files[0]})
+        const data = new FormData();
+        Object.keys(editClient).forEach(value => {
+            data.append(value, editClient[value])
+        });
+        dispatch(editUser(data, props.match.params.id))
+    };
 
-  const handleClickOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
-  return (
-    <Grid container justify="center">
-      <Grid item xs={12} md={10} lg={8}>
-        <Box ml={4}>
-          <Box pt={2} pb={2}>
-            <Typography variant="h4">Edit User</Typography>
-          </Box>
-          {state && <Grid container direction="column" spacing={2}>
-            <Grid item xs>
-              <TextField type="text"
-                         value={state.username}
-                         name="username"
-                         variant="outlined"
-                         label="Change username"
-                         onChange={changeInputHandler}
-                         fullWidth
-              />
+    return (
+        <Container>
+            <Grid style={{margin: '0 auto', marginTop: '5%'}} item xs={12} lg={8} sm={7} ml={8}>
+                <Box component="div" boxShadow={10} p={5}>
+                    <Box className={classes.typography} component={'span'}>
+                        <Typography className={classes.typographyText} variant="h6" gutterBottom>
+                            Редактирование пользователя
+                        </Typography>
+                    </Box>
+                    <form onSubmit={onSubmit}>
+                        {editClient && <Grid container direction='column' spacing={1}>
+                            <Grid item>
+                                <FormElement
+                                    required
+                                    propertyName='username'
+                                    title='Логин'
+                                    value={editClient.username}
+                                    onChange={inputChangeHandler}
+                                />
+                            </Grid>
+                            <Grid item>
+                                <FormElement
+                                    propertyName='password'
+                                    title='Пароль'
+                                    onChange={inputChangeHandler}
+                                />
+                            </Grid>
+                            <Grid item>
+                                <FormElement
+                                    propertyName='displayName'
+                                    title='Отображаемое имя'
+                                    value={editClient.displayName}
+                                    onChange={inputChangeHandler}
+                                />
+                            </Grid>
+                            <Grid item>
+                                <FormElement
+                                    propertyName='role'
+                                    title='Роль'
+                                    value={editClient.role}
+                                    onChange={inputChangeHandler}
+                                    type='select'
+                                    options={roles}
+                                />
+                            </Grid>
+                            {editClient.role === 'market' && <>
+                                <Grid item>
+                                    <FormElement
+                                        propertyName='companyName'
+                                        title='Название компании'
+                                        value={editClient.companyName}
+                                        onChange={inputChangeHandler}
+                                    />
+                                </Grid>
+                                <Grid item>
+                                    <FormElement
+                                        propertyName='address'
+                                        title='Адрес компании'
+                                        value={editClient.address}
+                                        onChange={inputChangeHandler}
+                                    />
+                                </Grid>
+                            </>}
+                            <Grid item>
+                                <FormElement
+                                    propertyName='avatar'
+                                    title='avatar'
+                                    value={editClient.avatar}
+                                    onChange={fileChangeHandler}
+                                    type='file'
+                                />
+                            </Grid>
+                            <Grid item>
+                                <MuiPhoneNumber
+                                    required
+                                    className={classes.phoneInput}
+                                    defaultCountry={'kg'}
+                                    value={editClient.phone}
+                                    onChange={phoneChangeHandler}
+                                />
+                            </Grid>
+                            {error && <Grid item>
+                                <Alert severity='error'>{error}</Alert>
+                            </Grid>}
+                            <Grid item>
+                                <Box className={classes.formBtn} component="span">
+                                    <Button
+                                        className={classes.formButton}
+                                        variant='contained'
+                                        color='primary'
+                                        type='submit'
+                                    >
+                                        Редактировать
+                                    </Button>
+                                </Box>
+                            </Grid>
+                            <Grid item>
+                                <Button
+                                    variant="contained"
+                                    color="secondary"
+                                    startIcon={<DeleteIcon/>}
+                                    onClick={handleClickOpen}
+                                >
+                                    Удалить пользователя
+                                </Button>
+                            </Grid>
+                        </Grid>}
+                    </form>
+                </Box>
             </Grid>
-            <Grid item xs>
-              <TextField type="text"
-                         value={state.displayName}
-                         name="displayName"
-                         label="Change display name"
-                         variant="outlined"
-                         onChange={changeInputHandler}
-                         fullWidth
-              />
-            </Grid>
-            <Grid item xs>
-              <TextField type="text"
-                         label="password"
-                         name="password"
-                         variant="outlined"
-                         onChange={changeInputHandler}
-                         fullWidth
-              />
-            </Grid>
-            <Grid item xs>
-              <TextField type="file"
-                         name="avatar"
-                         variant="outlined"
-                         onChange={changeFileHandler}
-                         fullWidth
-              />
-            </Grid>
-            <Grid item xs>
-              <InputLabel id="demo-simple-select-label">Role</InputLabel>
-              <select name="role" value={state.role} onChange={changeInputHandler}>
-                {ROLES.map(role => (
-                  <option value={role} key={role}>{role}</option>
-                ))}
-              </select>
-            </Grid>
-            {state.role === 'market' && (
-              <>
-                <Grid item xs>
-                  <TextField type="text"
-                             label="address"
-                             name="address"
-                             variant="outlined"
-                             onChange={changeInputHandler}
-                             fullWidth
-                  />
-                </Grid>
-                <Grid item xs>
-                  <TextField type="text"
-                             label="market name"
-                             name="marketName"
-                             variant="outlined"
-                             onChange={changeInputHandler}
-                             fullWidth
-                  />
-                </Grid>
-              </>
-            )}
-            <Grid container spacing={2}>
-              <Grid item>
-                <Button
-                  color="primary"
-                  variant="contained"
-                  onClick={editProfile}
-                >
-                  Сохронить изменения
-                </Button>
-              </Grid>
-              <Grid item>
-                <Button
-                  variant="contained"
-                  color="secondary"
-                  startIcon={<DeleteIcon/>}
-                  onClick={handleClickOpen}
-                >
-                  Удалить пользователя
-                </Button>
-              </Grid>
-            </Grid>
-          </Grid>}
-        </Box>
-      </Grid>
-      <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
-        <DialogTitle id="simple-dialog-title">Вы уверены что хотите удалить этого пользователя?</DialogTitle>
-        <DialogContent>
-          {error && <Box mb={1}>
-            <Alert severity="error">{error}</Alert>
-          </Box>}
-          <Grid container justify='flex-end' spacing={1}>
-            <Grid item>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={handleClose}
-              >нет</Button>
-            </Grid>
-            <Grid item>
-              <Button
-                variant="contained"
-                color="secondary"
-                onClick={removeUser}
-              >да</Button>
-            </Grid>
-          </Grid>
-        </DialogContent>
-      </Dialog>
-    </Grid>
-  );
+            <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
+                <DialogTitle id="simple-dialog-title">Вы уверены что хотите удалить этого пользователя?</DialogTitle>
+                <DialogContent>
+                    {error && <Box mb={1}>
+                        <Alert severity="error">{error}</Alert>
+                    </Box>}
+                    <Grid container justify='flex-end' spacing={1}>
+                        <Grid item>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                onClick={handleClose}
+                            >нет</Button>
+                        </Grid>
+                        <Grid item>
+                            <Button
+                                variant="contained"
+                                color="secondary"
+                                onClick={removeUser}
+                            >да</Button>
+                        </Grid>
+                    </Grid>
+                </DialogContent>
+            </Dialog>
+        </Container>
+    );
 };
 
 export default EditUser;
