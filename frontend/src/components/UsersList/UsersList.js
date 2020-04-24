1489f09b532e45ca1515d5e1c83d69bde969f5fa
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-04-24 15:36:20 +0600

    Исправил отображение саиска пользователей

--- frontend/src/components/UsersList/UsersList.js
+++ frontend/src/components/UsersList/UsersList.js
@@ -5,8 +5,14 @@ import Button from "@material-ui/core/Button";
 import {NavLink} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {getUsers} from "../../store/actions/usersActions";
-import List from "@material-ui/core/List";
-import UserItem from "./UserListItem/UserListItem";
+import UserListItem from "./UserListItem/UserListItem";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableBody from "@material-ui/core/TableBody";
+import Paper from "@material-ui/core/Paper";
 
 const UsersList = () => {
     const dispatch = useDispatch()
@@ -33,17 +39,37 @@ const UsersList = () => {
                     </Button>
                 </Grid>
             </Grid>
-            <List>
-                {users && users.map((user, index) => {
-                    return (
-                        <UserItem
-                            displayName={user.displayName}
-                            number={index+1}
-                            role={user.role}
-                        />
-                    )
-                })}
-            </List>
+            <Grid item>
+                <TableContainer component={Paper}>
+                    <Table aria-label="caption table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell><b>Имя</b></TableCell>
+                                <TableCell><b>Логин</b></TableCell>
+                                <TableCell><b>Телефон</b></TableCell>
+                                <TableCell><b>Название компании</b></TableCell>
+                                <TableCell><b>Адрес</b></TableCell>
+                                <TableCell><b>Роль</b></TableCell>
+                                <TableCell> </TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {users && users.map(user => (
+                              <UserListItem
+                                key={user._id}
+                                id={user._id}
+                                displayName={user.displayName}
+                                username={user.username}
+                                phone={user.phone}
+                                companyName={user.companyName}
+                                address={user.address}
+                                role={user.role}
+                              />
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Grid>
         </Box>
     );
 };
