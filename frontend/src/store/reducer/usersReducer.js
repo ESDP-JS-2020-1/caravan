608980653efe97b2b4af8209f6f8ce5e2640711a
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-04-24 19:01:25 +0600

    Добавил обработку екшенов удаления пользователя

--- frontend/src/store/reducer/usersReducer.js
+++ frontend/src/store/reducer/usersReducer.js
@@ -1,5 +1,5 @@
 import {
-  ADD_USER_FAILURE, GET_EDIT_USER_FAILURE, GET_EDIT_USER_REQUEST, GET_EDIT_USER_SUCCESS,
+  ADD_USER_FAILURE, DELETE_USER_FAILURE, GET_EDIT_USER_FAILURE, GET_EDIT_USER_SUCCESS,
   GET_USERS_FAILURE,
   GET_USERS_REQUEST,
   GET_USERS_SUCCESS,
@@ -22,6 +22,10 @@ const initialState = {
 
 const usersReducer = (state = initialState, action) => {
   switch (action.type) {
+    case GET_EDIT_USER_SUCCESS:
+      return {...state, editUser: action.user}
+    case GET_EDIT_USER_FAILURE:
+      return {...state, error: action.error}
     case REGISTER_USER_REQUEST:
       return {...state, registerLoading: true};
     case REGISTER_USER_SUCCESS:
@@ -44,10 +48,8 @@ const usersReducer = (state = initialState, action) => {
       return {...state, error: action.error}
     case ADD_USER_FAILURE:
       return {...state, error: action.error.response.data.message}
-    case GET_EDIT_USER_SUCCESS:
-      return {...state, editUser: action.user}
-    case GET_EDIT_USER_FAILURE:
-      return {...state, error: action.error}
+    case DELETE_USER_FAILURE:
+      return {...state, error: action.error.response.data.message}
 
     default:
       return state;
