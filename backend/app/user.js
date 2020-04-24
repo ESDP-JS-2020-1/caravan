a8da3d85b2550d6a72e625b313a4e441b5d9121a
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-04-24 13:33:47 +0600

    Добавил путь users/:id для удаления пользователя администратором

--- backend/app/user.js
+++ backend/app/user.js
@@ -14,92 +14,108 @@ const permit = require('../middleware/permit');
 const router = express.Router();
 
 const storage = multer.diskStorage({
-    destination: (req, file, cd) => {
-        cd(null, config.userAvatar)
-    },
-    filename: (req, file, cd) => {
-        cd(null, nanoid() + path.extname(file.originalname));
-    }
+  destination: (req, file, cd) => {
+    cd(null, config.userAvatar)
+  },
+  filename: (req, file, cd) => {
+    cd(null, nanoid() + path.extname(file.originalname));
+  }
 });
 
 const upload = multer({storage});
 
 router.post('/', isAuth, permit('admin'), upload.single('avatar'), async (req, res) => {
-    const user = req.body;
+  const user = req.body;
 
-    if (req.file) {
-        req.body.avatar = req.file.filename
-    }
+  if (req.file) {
+    req.body.avatar = req.file.filename
+  }
 
-    const newUser = new User({
-        username: user.username,
-        password: user.password,
-        displayName: user.displayName,
-        role: user.role,
-        avatar: user.avatar,
-        address: user.address,
-        companyName: user.companyName,
-        phone: user.phone
-    });
+  const newUser = new User({
+    username: user.username,
+    password: user.password,
+    displayName: user.displayName,
+    role: user.role,
+    avatar: user.avatar,
+    address: user.address,
+    companyName: user.companyName,
+    phone: user.phone
+  });
 
-    try {
-        newUser.addToken();
-        await newUser.save();
+  try {
+    newUser.addToken();
+    await newUser.save();
 
-        res.send(newUser)
-    } catch (e) {
-        res.status(404).send(e)
-    }
+    res.send(newUser)
+  } catch (e) {
+    res.status(404).send(e)
+  }
 });
 
 router.get('/', isAuth, async (req, res) => {
-    try {
-        const users = await User.find().select({token: 0});
+  try {
+    const users = await User.find().select({token: 0});
 
-        res.send(users)
-    } catch (e) {
-        res.status(500).send(e)
-    }
+    res.send(users)
+  } catch (e) {
+    res.status(500).send(e)
+  }
 });
+
+router.delete('/:id', isAuth, permit('admin'), async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const user = await User.findOne({_id: id})
+
+    if(!user) return res.status(404).send("User not found");
+
+    await User.deleteOne({_id: user._id})
+
+    res.send('success')
+  } catch (e) {
+    res.status(500).send(e)
+  }
+})
 
 router.post('/sessions', async (req, res) => {
 
     const user = await User.findOne({username: req.body.username});
     if (!user) {
-        res.status(404).send({message: 'Username or password not correct!'});
+      res.status(404).send({message: 'Username or password not correct!'});
     } else {
-        const correctPassword = await bcrypt.compare(req.body.password, user.password);
-        if (!correctPassword) {
-            return res.status(404).send({message: 'Username or password not correct!'});
-        }
+      const correctPassword = await bcrypt.compare(req.body.password, user.password);
+      if (!correctPassword) {
+        return res.status(404).send({message: 'Username or password not correct!'});
+      }
     }
 
-        user.addToken();
-        user.save();
+    user.addToken();
+    user.save();
 
-        res.send(user)
-    }
+    res.send(user)
+  }
 );
 
 router.delete('/sessions', async (req, res) => {
-    const success = {message: "success"};
-    try {
-        const token = req.get('Authorization').split(' ')[1];
+  const success = {message: "success"};
+  try {
+    const token = req.get('Authorization').split(' ')[1];
 
-        if (!token) return res.send(success);
+    if (!token) return res.send(success);
 
-        const user = await User.findOne({token});
+    const user = await User.findOne({token});
 
-        if (!user) return res.send(success);
+    if (!user) return res.send(success);
 
-        user.addToken();
-        await user.save();
+    user.addToken();
+    await user.save();
 
-        return res.send(success);
+    return res.send(success);
 
-    } catch (e) {
-        res.send(success)
-    }
+  } catch (e) {
+    res.send(success)
+  }
 
 });
 
