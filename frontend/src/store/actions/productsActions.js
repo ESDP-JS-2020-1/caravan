a5bb6035a4c7f826871b1fb375f3447e5d326530
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-04-25 13:55:34 +0600

    Исправил екшены добавление продукта

--- frontend/src/store/actions/productsActions.js
+++ frontend/src/store/actions/productsActions.js
@@ -8,14 +8,13 @@ export const createProductRequest = () => ({type: CREATE_PRODUCT_REQUEST});
 export const createProductSuccess = () => ({type: CREATE_PRODUCT_SUCCESS});
 export const createProductError = error => ({type: CREATE_PRODUCT_ERROR, error});
 
-export const addNewProduct = productData => {
-    return async (dispatch, getState) => {
-        try {
-            const token = getState().users.user;
-            const headers = {'Authorization': 'Token ' + token};
-            await axiosApi.post('/products', productData, {headers});
-        } catch (error) {
-            dispatch(createProductError(error))
-        }
-    }
+export const addNewProduct = productData => async (dispatch) => {
+  try {
+    dispatch(createProductRequest())
+    await axiosApi.post('/products', productData);
+
+    dispatch(createProductSuccess())
+  } catch (error) {
+    dispatch(createProductError(error))
+  }
 };
