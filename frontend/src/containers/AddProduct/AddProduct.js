57dfd39e0466a8f47880758e08ab724b813cf505
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-04-25 12:13:55 +0600

    Улучшил стилезацию полей форм добавления нового продукта

--- frontend/src/containers/AddProduct/AddProduct.js
+++ frontend/src/containers/AddProduct/AddProduct.js
@@ -14,180 +14,180 @@ import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import IconButton from "@material-ui/core/IconButton";
 
 const useStyles = makeStyles((theme) => ({
-    heading: {
-        fontSize: theme.typography.pxToRem(15),
-        fontWeight: theme.typography.fontWeightRegular,
-    },
-    formBtn: {
-        marginTop: '1%',
-        display: 'flex',
-        justifyContent: 'space-between',
-        textAlign: 'center',
-    },
-    formButton: {
-        fontWeight: 'bold',
-        width: '49%',
-        minHeight: '50px'
-    },
-    typography: {
-        color: '#0d47a1',
-        textAlign: 'center',
-    },
-    typographyText: {
-        borderBottom: '2px solid #0d47a1',
-        textTransform: 'uppercase',
-        fontWeight: 'bold',
-        marginBottom: '3%',
-    },
-    phoneInput: {
-        width: '100%',
-    }
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+  formBtn: {
+    marginTop: '1%',
+    display: 'flex',
+    justifyContent: 'space-between',
+    textAlign: 'center',
+  },
+  formButton: {
+    fontWeight: 'bold',
+    width: '49%',
+    minHeight: '50px'
+  },
+  typography: {
+    color: '#0d47a1',
+    textAlign: 'center',
+  },
+  typographyText: {
+    borderBottom: '2px solid #0d47a1',
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+    marginBottom: '3%',
+  },
+  phoneInput: {
+    width: '100%',
+  }
 }));
 const AddProduct = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const [product, setProduct] = useState([{
-        name: '',
-        amount: '',
-        price: '',
-    }]);
+  const [product, setProduct] = useState([{
+    name: '',
+    amount: '',
+    price: '',
+  }]);
 
-    const inputChangeHandler = (e, i) => {
-        let newProduct = [...product];
-        newProduct[i][e.target.name] = e.target.value;
-        setProduct(newProduct)
-    };
+  const inputChangeHandler = (e, i) => {
+    let newProduct = [...product];
+    newProduct[i][e.target.name] = e.target.value;
+    setProduct(newProduct)
+  };
 
-    const addProduct = (e) => {
-        e.preventDefault();
+  const addProduct = (e) => {
+    e.preventDefault();
 
-        const newProduct = product[0] ? [...product, {
-            name: '',
-            amount: '',
-            price: '',
-        }] : [{
-            name: '',
-            amount: '',
-            price: '',
-        }];
+    const newProduct = product[0] ? [...product, {
+      name: '',
+      amount: '',
+      price: '',
+    }] : [{
+      name: '',
+      amount: '',
+      price: '',
+    }];
 
-        setProduct(newProduct)
-    };
+    setProduct(newProduct)
+  };
 
-    const deleteProduct = id => {
-        const products = [...product];
-       products.splice(id, 1);
+  const deleteProduct = id => {
+    const products = [...product];
+    products.splice(id, 1);
 
-        setProduct(products);
-    };
+    setProduct(products);
+  };
 
-    const onSubmit = e => {
-        e.preventDefault();
+  const onSubmit = e => {
+    e.preventDefault();
 
-    };
+  };
 
-    const [expanded, setExpanded] = React.useState(false);
+  const [expanded, setExpanded] = React.useState(false);
 
-    const handleChange = (panel) => (event, isExpanded) => {
-        setExpanded(isExpanded ? panel : false);
-    };
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
 
-    return (
-        <Container>
-            <Grid style={{margin: '0 auto', marginTop: '5%'}} item xs={12} lg={8} sm={7} ml={8}>
-                <Box component="div" boxShadow={10} p={5}>
-                    <Box className={classes.typography} component={'span'}>
-                        <Typography className={classes.typographyText} variant="h6" gutterBottom>
-                            Добовление продуктов
+  return (
+    <Container>
+      <Grid style={{margin: '0 auto', marginTop: '5%'}} item xs={12} lg={8} sm={7} ml={8}>
+        <Box component="div" boxShadow={10} p={5}>
+          <Box className={classes.typography} component={'span'}>
+            <Typography className={classes.typographyText} variant="h6" gutterBottom>
+              Добовление продуктов
+            </Typography>
+          </Box>
+          <form onSubmit={onSubmit}>
+            <Grid container direction='column' spacing={1}>
+
+              {product.map((p, i) => (
+                <Grid container key={i} alignItems='center' spacing={1}>
+                  <Grid item xs={11}>
+                    <ExpansionPanel expanded={expanded === 'panel' + i}
+                                    onChange={handleChange('panel' + i)}>
+                      <ExpansionPanelSummary
+                        expandIcon={<ExpandMoreIcon/>}
+                        aria-controls="panel1a-content"
+                        id={`panel${i}bh-header`}
+                      >
+                        <Typography className={classes.heading}>
+                          Название: {p.name}
+                          Количество: {p.amount}
                         </Typography>
-                    </Box>
-                    <form onSubmit={onSubmit}>
-                        <Grid container direction='column' spacing={1}>
+                      </ExpansionPanelSummary>
+                      <ExpansionPanelDetails>
+                        <Box style={{width: '100%', marginBottom: "10px"}}>
+                          <Grid item>
+                            <FormElement
+                              required
+                              propertyName='name'
+                              title='Название'
+                              value={product[i].name}
+                              onChange={(e) => inputChangeHandler(e, i)}
+                            />
+                          </Grid>
+                          <Grid item>
+                            <FormElement
+                              required
+                              propertyName='amount'
+                              title='Количество'
+                              value={product[i].amount}
+                              onChange={(e) => inputChangeHandler(e, i)}
+                            />
+                          </Grid>
+                          <Grid item>
+                            <FormElement
+                              propertyName='price'
+                              title='Цена'
+                              value={product[i].price}
+                              onChange={(e) => inputChangeHandler(e, i)}
+                            />
+                          </Grid>
+                        </Box>
+                      </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                  </Grid>
+                  <Grid item xs={1}>
+                    <IconButton aria-label="delete" onClick={() => deleteProduct(i)}>
+                      <DeleteIcon fontSize="large"/>
+                    </IconButton>
+                  </Grid>
+                </Grid>
+              ))}
 
-                            {product.map((p, i) => (
-                                <Grid container key={i}>
-                                    <Grid item xs={11}>
-                                        <ExpansionPanel expanded={expanded === 'panel' + i}
-                                                        onChange={handleChange('panel' + i)}>
-                                            <ExpansionPanelSummary
-                                                expandIcon={<ExpandMoreIcon/>}
-                                                aria-controls="panel1a-content"
-                                                id={`panel${i}bh-header`}
-                                            >
-                                                <Typography className={classes.heading}>
-                                                    Название: {p.name}
-                                                    Количество: {p.amount}
-                                                </Typography>
-                                            </ExpansionPanelSummary>
-                                            <ExpansionPanelDetails>
-                                                <Box style={{width: '100%', marginBottom: "10px"}}>
-                                                    <Grid item>
-                                                        <FormElement
-                                                            required
-                                                            propertyName='name'
-                                                            title='Название'
-                                                            value={product[i].name}
-                                                            onChange={(e) => inputChangeHandler(e, i)}
-                                                        />
-                                                    </Grid>
-                                                    <Grid item>
-                                                        <FormElement
-                                                            required
-                                                            propertyName='amount'
-                                                            title='Количество'
-                                                            value={product[i].amount}
-                                                            onChange={(e) => inputChangeHandler(e, i)}
-                                                        />
-                                                    </Grid>
-                                                    <Grid item>
-                                                        <FormElement
-                                                            propertyName='price'
-                                                            title='Цена'
-                                                            value={product[i].price}
-                                                            onChange={(e) => inputChangeHandler(e, i)}
-                                                        />
-                                                    </Grid>
-                                                </Box>
-                                            </ExpansionPanelDetails>
-                                        </ExpansionPanel>
-                                    </Grid>
-                                    <Grid item xs={1}>
-                                        <IconButton aria-label="delete" onClick={() => deleteProduct(i)}>
-                                            <DeleteIcon fontSize="large"/>
-                                        </IconButton>
-                                    </Grid>
-                                </Grid>
-                            ))}
-
-                            {/*{error && <Grid item>*/}
-                            {/*    <Alert severity='error'>{error}</Alert>*/}
-                            {/*</Grid>}*/}
-                            <Grid item>
-                                <Box className={classes.formBtn} component="span">
-                                    <Button
-                                        onClick={addProduct}
-                                        className={classes.formButton}
-                                        variant='contained'
-                                        color='primary'
-                                    >
-                                        Добавить
-                                    </Button>
-                                    <Button
-                                        className={classes.formButton}
-                                        variant='contained'
-                                        color='primary'
-                                        type='submit'
-                                    >
-                                        Сохранить
-                                    </Button>
-                                </Box>
-                            </Grid>
-                        </Grid>
-                    </form>
+              {/*{error && <Grid item>*/}
+              {/*    <Alert severity='error'>{error}</Alert>*/}
+              {/*</Grid>}*/}
+              <Grid item>
+                <Box className={classes.formBtn} component="span">
+                  <Button
+                    onClick={addProduct}
+                    className={classes.formButton}
+                    variant='contained'
+                    color='primary'
+                  >
+                    Добавить
+                  </Button>
+                  <Button
+                    className={classes.formButton}
+                    variant='contained'
+                    color='primary'
+                    type='submit'
+                  >
+                    Сохранить
+                  </Button>
                 </Box>
+              </Grid>
             </Grid>
-        </Container>
-    );
+          </form>
+        </Box>
+      </Grid>
+    </Container>
+  );
 };
 
 export default AddProduct;
