d4632007c49c059b5d88c9ca6f93bba66c29145d
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-04-25 13:34:44 +0600

    Переделал путь добавления продукта

--- backend/app/products.js
+++ backend/app/products.js
@@ -8,11 +8,22 @@ const permit = require('../middleware/permit');
 const router = express.Router();
 
 router.post('/', [auth, permit('admin')], async (req, res) => {
-    const product = new Product({products: req.body});
-
     try {
-        await product.save();
-        return res.send(product);
+        const products = req.body;
+
+        await Promise.all(products.map(async (product, index) => {
+            if(!products[index].name || !products[index].amount || !products[index].price) {
+                return res.status(404).send({message: 'All fields must be filled'})
+            }
+
+            await Product.create({
+                name: products[index].name,
+                amount: products[index].amount,
+                price: products[index].price,
+            });
+        }))
+
+        return res.send({message: 'success'});
     } catch (e) {
         res.status(400).send(e);
     }
