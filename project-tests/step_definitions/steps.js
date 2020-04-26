0d33dc577a1195c40354d62246eef105281bced4
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-04-26 21:48:40 +0600

    Реализовал шаги тестов по удалению пользователя

--- project-tests/step_definitions/steps.js
+++ project-tests/step_definitions/steps.js
@@ -1,144 +1,172 @@
-const { I } = inject();
+const {I} = inject();
 
 Given('я нахожусь на странице логина', () => {
   I.amOnPage('/login');
 });
 
 When('я заполняю поля формы:', table => {
-	const tableData = table.parse().rawData;
+  const tableData = table.parse().rawData;
 
-	tableData.forEach(row => {
-		I.fillField(row[0], row[1]);
-	})
+  tableData.forEach(row => {
+    I.fillField(row[0], row[1]);
+  })
 });
 
 When('нажимаю на кнопку {string}', btnName => {
-	I.click(btnName, '#btn');
+  I.click(btnName, '#btn');
 });
 
 When('я вижу текст {string}', text => {
- I.waitForText(text);
+  I.waitForText(text);
 });
 
 When('я залогинен как администратор:', table => {
-	I.amOnPage('/login');
+  I.amOnPage('/login');
 
-	const tableData = table.parse().rawData;
+  const tableData = table.parse().rawData;
 
-	tableData.forEach(row => {
-		I.fillField(row[0], row[1]);
-	});
+  tableData.forEach(row => {
+    I.fillField(row[0], row[1]);
+  });
 
-	I.click("Login", '#btn');
+  I.click("Login", '#btn');
 
-	I.waitForText('Вы успешно вошли, как Some market');
+  I.waitForText('Вы успешно вошли, как Some market');
 });
 
 When('я нахожусь на корневой странице', () => {
-	I.amOnPage('/');
+  I.amOnPage('/');
 });
 
 When('я нажму на иконку юзера {string}', btnName => {
-	I.click(btnName);
+  I.click(btnName);
 });
 
 When('я нажму на кнопку {string}', btnName => {
-	I.click(btnName);
+  I.click(btnName);
 });
 
 When('я попадаю на страницу со списком пользователей', () => {
-	I.amOnPage('/users');
+  I.amOnPage('/users');
 });
 
 When('если я нажму на кнопку {string}', btnName => {
-	I.click(btnName);
+  I.click(btnName);
 });
 
 When('я попаду на страницу добавления нового пользователя', () => {
-	I.amOnPage('/users/new');
+  I.amOnPage('/users/new');
 });
 
-When('я заполняю поля формы добавления:',   table => {
-	const tableData = table.parse().rawData;
+When('я заполняю поля формы добавления:', table => {
+  const tableData = table.parse().rawData;
 
-	tableData.forEach(row => {
-		I.fillField(row[0], row[1]);
-	});
+  tableData.forEach(row => {
+    I.fillField(row[0], row[1]);
+  });
 });
 
 When('выбираю роль пользователя {string}', role => {
-	I.click("#role");
-	I.click(`#${role}Option`);
+  I.click("#role");
+  I.click(`#${role}Option`);
 });
 
 When('я заполняю поля формы для названия компании и адреса :', table => {
-	const tableData = table.parse().rawData;
+  const tableData = table.parse().rawData;
 
-	tableData.forEach(row => {
-		I.fillField(row[0], row[1]);
-	});
+  tableData.forEach(row => {
+    I.fillField(row[0], row[1]);
+  });
 });
 
 When('нажимаю на кнопку  {string}', btnName => {
-	I.click(btnName);
+  I.click(btnName);
 });
 
 When('я вижу текст после добавления {string}', text => {
-	I.waitForText(text);
+  I.waitForText(text);
 });
-
-
 
 
 Given('я залогинен как администратор:', table => {
-	I.amOnPage('/login');
+  I.amOnPage('/login');
 
-	const tableData = table.parse().rawData;
+  const tableData = table.parse().rawData;
 
-	tableData.forEach(row => {
-		I.fillField(row[0], row[1]);
-	});
+  tableData.forEach(row => {
+    I.fillField(row[0], row[1]);
+  });
 
-	I.click("Login", '#btn');
+  I.click("Login", '#btn');
 
-	I.waitForText('Вы успешно вошли, как Some market');
+  I.waitForText('Вы успешно вошли, как Some market');
 });
 
 When('я нахожусь на корневой странице', () => {
-	I.amOnPage('/');
+  I.amOnPage('/');
 });
 
 When('я нажму на иконку юзера {string}', btnName => {
-	I.click(btnName);
+  I.click(btnName);
 });
 
 When('я нажму на кнопку {string}', btnName => {
-	I.click(btnName);
+  I.click(btnName);
 });
 
 
 When('я попадаю на страницу добавления продукта', () => {
-	I.amOnPage('/product/add');
+  I.amOnPage('/product/add');
 });
 
 When('я кликаю на кнопку {string}', (btnName) => {
-	I.click(btnName, '#panel0bh-header');
+  I.click(btnName, '#panel0bh-header');
 });
 
 When('я заполняю поля формы добавления', table => {
-	const tableData = table.parse().rawData;
+  const tableData = table.parse().rawData;
 
-	tableData.forEach(row => {
-		I.fillField(row[0], row[1]);
-	});
+  tableData.forEach(row => {
+    I.fillField(row[0], row[1]);
+  });
 });
 
 When('если я нажму на кнопку {string}', btnName => {
-	I.click(btnName);
+  I.click(btnName);
 });
 
 Then('я вижу сообщение {string}', (message) => {
-	I.waitForText(message);
+  I.waitForText(message);
+});
+
+
+Given('я зарегестрирован как администратор', table => {
+  I.amOnPage('/login');
+
+  const tableData = table.parse().rawData;
+
+  tableData.forEach(row => {
+    I.fillField(row[0], row[1]);
+  });
+
+  I.click("Login", '#btn');
+
+  I.waitForText('Вы успешно вошли, как Some market');
+  I.wait(1)
+});
+
+When('я нахожусь на главной странице', () => {
+  I.amOnPage('/');
+  I.wait(1)
+});
+
+When('я нажму на кнопку {string}', btnName => {
+  I.click(btnName);
+  I.wait(3)
+});
+
+When('я вижу текст после удаления {string}', text => {
+  I.waitForText(text);
 });
 
 
