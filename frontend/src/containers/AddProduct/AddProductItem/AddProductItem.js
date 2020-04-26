01d6e8fd1518bdae2fd98b0ae871397bbb6ef3ac
Ulan Beltaev <ulanbeltaev@gmail.com>
2020-04-26 20:08:18 +0600

    Добавил id для полей формы

--- frontend/src/containers/AddProduct/AddProductItem/AddProductItem.js
+++ frontend/src/containers/AddProduct/AddProductItem/AddProductItem.js
@@ -11,62 +11,66 @@ import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from "@material-ui/icons/Delete";
 
 const AddProductItem = (
-  {onChange, index, expanded, classes, p, product, onRemove, handleChange}
+	{onChange, index, expanded, classes, p, product, onRemove, handleChange}
 ) => {
-  return (
-    <Grid container alignItems='center' spacing={1}>
-      <Grid item xs={11}>
-        <ExpansionPanel expanded={expanded === 'panel' + index}
-                        onChange={handleChange('panel' + index)}>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon/>}
-            aria-controls="panel1a-content"
-            id={`panel${ index }bh-header`}
-          >
-            <Typography className={classes.heading}>
-              Название: {p.name}
-              Количество: {p.amount}
-            </Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Box style={{width: '100%', marginBottom: "10px"}}>
-              <Grid item>
-                <FormElement
-                  required
-                  propertyName='name'
-                  title='Название'
-                  value={product[index].name}
-                  onChange={(e) => onChange(e, index)}
-                />
-              </Grid>
-              <Grid item>
-                <FormElement
-                  required
-                  propertyName='amount'
-                  title='Количество'
-                  value={product[index].amount}
-                  onChange={(e) => onChange(e, index)}
-                />
-              </Grid>
-              <Grid item>
-                <FormElement
-                  propertyName='price'
-                  title='Цена'
-                  value={product[index].price}
-                  onChange={(e) => onChange(e, index)}
-                />
-              </Grid>
-            </Box>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-      </Grid>
-      <Grid item xs={1}>
-        <IconButton aria-label="delete" onClick={() => onRemove(index)}>
-          <DeleteIcon fontSize="large"/>
-        </IconButton>
-      </Grid>
-    </Grid>
-  );
+	return (
+		<Grid container alignItems='center' spacing={1}>
+			<Grid item xs={11}>
+				<ExpansionPanel expanded={expanded === 'panel' + index}
+								onChange={handleChange('panel' + index)}
+                >
+					<ExpansionPanelSummary
+						expandIcon={<ExpandMoreIcon/>}
+						aria-controls="panel1a-content"
+						id={`panel${index}bh-header`}
+					>
+						<Typography className={classes.heading}>
+							Название: {p.name}
+							Количество: {p.amount}
+						</Typography>
+					</ExpansionPanelSummary>
+					<ExpansionPanelDetails>
+						<Box style={{width: '100%', marginBottom: "10px"}}>
+							<Grid item>
+								<FormElement
+									id='name'
+									required
+									propertyName='name'
+									title='Название'
+									value={product[index].name}
+									onChange={(e) => onChange(e, index)}
+								/>
+							</Grid>
+							<Grid item>
+								<FormElement
+									id='amount'
+									required
+									propertyName='amount'
+									title='Количество'
+									value={product[index].amount}
+									onChange={(e) => onChange(e, index)}
+								/>
+							</Grid>
+							<Grid item>
+								<FormElement
+									id='price'
+									propertyName='price'
+									title='Цена'
+									value={product[index].price}
+									onChange={(e) => onChange(e, index)}
+								/>
+							</Grid>
+						</Box>
+					</ExpansionPanelDetails>
+				</ExpansionPanel>
+			</Grid>
+			<Grid item xs={1}>
+				<IconButton aria-label="delete" onClick={() => onRemove(index)}>
+					<DeleteIcon fontSize="large"/>
+				</IconButton>
+			</Grid>
+		</Grid>
+	);
 };
 
 export default AddProductItem;
