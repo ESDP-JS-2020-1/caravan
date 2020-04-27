10da3263e164ae733123710bcc9c8530410417e6
spartak <fomenkospartak@gmail.com>
2020-04-27 12:31:19 +0600

    исправил добавление продуктов на бэкенде

--- backend/app/products.js
+++ backend/app/products.js
@@ -11,17 +11,7 @@ router.post('/', [auth, permit('admin')], async (req, res) => {
     try {
         const products = req.body;
 
-        for(let i = 0; i < products.length; i++) {
-            if(!products[i].name || !products[i].amount || !products[i].price) {
-                return res.status(404).send({message: 'All fields must be filled'})
-            }
-
-            await Product.create({
-                name: products[i].name,
-                amount: products[i].amount,
-                price: products[i].price,
-            });
-        }
+        await Product.insertMany(products);
 
         return res.send({message: 'success'});
     } catch (e) {
