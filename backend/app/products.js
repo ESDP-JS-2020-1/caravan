29ac76c265b9dbb63fd0cb51634a1bd2c78e9350
ramazan <assasins11122001@gmail.com>
2020-04-27 21:16:18 +0600

    Добавил в route для Product multer

--- backend/app/products.js
+++ backend/app/products.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const multer = require('multer');
+const {nanoid} = require('nanoid');
+const path = require('path');
 
 const Product = require('../models/Product');
 const History = require('../models/History');
 
+const config = require('../config');
 const auth = require('../middleware/isAuth');
 const permit = require('../middleware/permit');
 
 const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: (req, file, cd) => {
+        cd(null, config.productImage)
+    },
+    filename: (req, file, cd) => {
+        cd(null, nanoid() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({storage});
 
 router.get('/', auth, async (req, res) => {
     try {
@@ -18,11 +33,14 @@ router.get('/', auth, async (req, res) => {
     }
 });
 
-router.post('/', [auth, permit('admin')], async (req, res) => {
+router.post('/', [auth, permit('admin'), upload.single('image')], async (req, res) => {
     try {
+        if (req.file) {
+            req.body.image = req.file.filename
+        }
         const products = req.body;
 
-        await Product.insertMany(products);
+        await Product.create(products);
 
         const history = new History({
             title: req.currentUser.displayName + ' добавил продукт ' + products.name,
