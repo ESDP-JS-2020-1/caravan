95f5f69ee05a772f54c99d9509cae154ea6d177b
ramazan <assasins11122001@gmail.com>
2020-04-27 13:10:48 +0600

    Добавил обработку экшенов получения всех продуктов

--- frontend/src/store/reducer/productsReducer.js
+++ frontend/src/store/reducer/productsReducer.js
@@ -1,15 +1,28 @@
-import {CREATE_PRODUCT_ERROR, CREATE_PRODUCT_INIT} from "../actions/productsActions";
+import {
+  CREATE_PRODUCT_ERROR,
+  CREATE_PRODUCT_INIT, GET_PRODUCTS_ERROR,
+  GET_PRODUCTS_REQUEST,
+  GET_PRODUCTS_SUCCESS
+} from "../actions/productsActions";
 
 const initialState = {
-  error: null
+  productsList: [],
+  loading: false,
+  error: null,
 };
 
 const productsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case GET_PRODUCTS_REQUEST:
+      return {...state, loading: true};
+    case GET_PRODUCTS_SUCCESS:
+      return {...state, productsList: action.products, loading: false};
+    case GET_PRODUCTS_ERROR:
+      return {...state, error: action.error, loading: false};
     case CREATE_PRODUCT_INIT:
-      return {...state, error: null}
+      return {...state, error: null};
     case CREATE_PRODUCT_ERROR:
-      return {...state, error: action.error.response.data.message}
+      return {...state, error: action.error.response.data.message};
     default: return state
   }
 };
