3873e8b0e7b5fe83780f6d9e436d3276518154a8
ramazan <assasins11122001@gmail.com>
2020-04-27 13:09:05 +0600

    Добавил route с получением всех продуктов

--- backend/app/products.js
+++ backend/app/products.js
@@ -7,6 +7,16 @@ const permit = require('../middleware/permit');
 
 const router = express.Router();
 
+router.get('/', auth, async (req, res) => {
+    try {
+        const products = await Product.find();
+
+        return res.send(products);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
 router.post('/', [auth, permit('admin')], async (req, res) => {
     try {
         const products = req.body;
