5a3176edb9518f665245bebc04e9cb30105591e4
spartak <fomenkospartak@gmail.com>
2020-04-28 01:22:47 +0600

    создал 2 экшина удаление и редактирования продукта

--- frontend/src/store/actions/productsActions.js
+++ frontend/src/store/actions/productsActions.js
@@ -13,6 +13,30 @@ export const GET_PRODUCTS_REQUEST = 'GET_PRODUCTS_REQUEST';
 export const GET_PRODUCTS_SUCCESS = 'GET_PRODUCTS_SUCCESS';
 export const GET_PRODUCTS_ERROR = 'GET_PRODUCTS_ERROR';
 
+export const GET_PRODUCT_REQUEST = 'GET_PRODUCT_REQUEST';
+export const GET_PRODUCT_SUCCESS = 'GET_PRODUCT_SUCCESS';
+export const GET_PRODUCT_FAILURE = 'GET_PRODUCT_FAILURE';
+
+export const EDIT_PRODUCT_REQUEST = 'EDIT_PRODUCT_REQUEST';
+export const EDIT_PRODUCT_SUCCESS = 'EDIT_PRODUCT_SUCCESS';
+export const EDIT_PRODUCT_FAILURE = 'EDIT_PRODUCT_FAILURE';
+
+export const DELETE_PRODUCT_REQUEST = 'DELETE_PRODUCT_REQUEST';
+export const DELETE_PRODUCT_SUCCESS = 'DELETE_PRODUCT_SUCCESS';
+export const DELETE_PRODUCT_FAILURE = 'DELETE_PRODUCT_FAILURE';
+
+export const deleteProductRequest = () => ({type: DELETE_PRODUCT_REQUEST});
+export const deleteProductSuccess = () => ({type: DELETE_PRODUCT_SUCCESS});
+export const deleteProductFailure = error => ({type: DELETE_PRODUCT_FAILURE});
+
+export const editProductRequest = () => ({type: EDIT_PRODUCT_REQUEST});
+export const editProductSuccess = () => ({type: EDIT_PRODUCT_SUCCESS});
+export const editProductFailure = (error) => ({type: EDIT_PRODUCT_FAILURE, error});
+
+export const getProductRequest = () => ({type: GET_PRODUCT_REQUEST});
+export const getProductSuccess = data => ({type: GET_PRODUCT_SUCCESS, data});
+export const getProductFailure = error => ({type: GET_PRODUCT_FAILURE, error});
+
 export const createProductRequest = () => ({type: CREATE_PRODUCT_REQUEST});
 export const createProductSuccess = () => ({type: CREATE_PRODUCT_SUCCESS});
 export const createProductError = error => ({type: CREATE_PRODUCT_ERROR, error});
@@ -21,42 +45,86 @@ export const getProductsRequest = () => ({type: GET_PRODUCTS_REQUEST});
 export const getProductsSuccess = products => ({type: GET_PRODUCTS_SUCCESS, products});
 export const getProductsError = error => ({type: GET_PRODUCTS_ERROR, error});
 
+
 export const createProductInit = () => ({type: CREATE_PRODUCT_INIT});
 
 export const getProductsList = () => async dispatch => {
-  try {
-    dispatch(getProductsRequest());
-    const products = await axiosApi.get('/products');
+    try {
+        dispatch(getProductsRequest());
+        const products = await axiosApi.get('/products');
 
-    dispatch(getProductsSuccess(products.data))
-  } catch (error) {
-    dispatch(getProductsError(error));
-  }
+        dispatch(getProductsSuccess(products.data))
+    } catch (error) {
+        dispatch(getProductsError(error));
+    }
 };
 
 export const addNewProduct = productData => async (dispatch) => {
-  try {
-    dispatch(createProductRequest());
+    try {
+        dispatch(createProductRequest());
+        await axiosApi.post('/products', productData);
 
-    const products = productData.map(async (elem) => {
-      const data = new FormData();
-      Object.keys(elem).forEach(value => {
-        data.append(value, elem[value])
-      });
-      return await axiosApi.post('/products', data);
-    });
+        dispatch(createProductSuccess());
 
-    await Promise.all(products);
+        notification.addNotification({
+            title: 'Добавление продукта',
+            message: `Продукт добавлен успешно`,
+            ...config.notification
+        });
+        dispatch(push('/product/list'))
+    } catch (error) {
+        dispatch(createProductError(error))
+    }
+};
 
-    dispatch(createProductSuccess());
+export const getProductEdit = id => {
+    return async dispatch => {
+        try {
+            dispatch(getProductRequest());
+            const response = await axiosApi.get('/products/' + id);
+            dispatch(getProductSuccess(response.data))
+        } catch (e) {
+            dispatch(getProductFailure(e))
+        }
 
-    notification.addNotification({
-      title: 'Добавление продукта',
-      message: `Продукт добавлен успешно`,
-      ...config.notification
-    });
-    dispatch(push('/products'))
-  } catch (error) {
-    dispatch(createProductError(error))
-  }
+    }
+};
+export const putEditProduct = (id, data) => {
+    return async dispatch => {
+        try {
+            dispatch(editProductRequest());
+            await axiosApi.put('/products/' + id, data);
+            dispatch(editProductSuccess());
+
+            notification.addNotification({
+                title: 'Редактирование',
+                message: `Продукт успешно изменён`,
+                ...config.notification
+            });
+            dispatch(push('/product/list'));
+        } catch (e) {
+            dispatch(editProductFailure(e))
+        }
+
+    }
+};
+export const deleteProduct = id => {
+
+    return async dispatch => {
+        try {
+            dispatch(deleteProductRequest());
+            await axiosApi.delete('/products/' + id);
+            dispatch(deleteProductSuccess());
+
+            notification.addNotification({
+                title: 'Редактирование',
+                message: `Продукт успешно удален`,
+                ...config.notification
+            });
+            dispatch(push('/product/list'));
+        } catch (e) {
+            dispatch(deleteProductFailure(e))
+        }
+
+    }
 };
--- frontend/src/store/reducer/productsReducer.js
+++ frontend/src/store/reducer/productsReducer.js
@@ -1,6 +1,11 @@
 import {
   CREATE_PRODUCT_ERROR,
-  CREATE_PRODUCT_INIT, GET_PRODUCTS_ERROR,
+  CREATE_PRODUCT_INIT, DELETE_PRODUCT_FAILURE, DELETE_PRODUCT_REQUEST, DELETE_PRODUCT_SUCCESS, EDIT_PRODUCT_FAILURE,
+  EDIT_PRODUCT_REQUEST, EDIT_PRODUCT_SUCCESS,
+  GET_PRODUCT_FAILURE,
+  GET_PRODUCT_REQUEST,
+  GET_PRODUCT_SUCCESS,
+  GET_PRODUCTS_ERROR,
   GET_PRODUCTS_REQUEST,
   GET_PRODUCTS_SUCCESS
 } from "../actions/productsActions";
@@ -9,6 +14,7 @@ const initialState = {
   productsList: [],
   loading: false,
   error: null,
+  editProduct:null,
 };
 
 const productsReducer = (state = initialState, action) => {
@@ -23,6 +29,24 @@ const productsReducer = (state = initialState, action) => {
       return {...state, error: null};
     case CREATE_PRODUCT_ERROR:
       return {...state, error: action.error.response.data.message};
+    case GET_PRODUCT_REQUEST:
+      return{...state,loading: true};
+    case GET_PRODUCT_SUCCESS:
+      return{...state,editProduct: action.data,loading: false};
+    case GET_PRODUCT_FAILURE:
+      return {...state,error:action.error,loading: false};
+    case EDIT_PRODUCT_REQUEST:
+      return{...state,loading: true};
+    case EDIT_PRODUCT_SUCCESS:
+      return{...state,loading: false};
+    case EDIT_PRODUCT_FAILURE:
+      return {...state,loading: false,error:action.error};
+    case DELETE_PRODUCT_REQUEST:
+      return{...state,loading: true};
+    case DELETE_PRODUCT_SUCCESS:
+      return{...state,loading: false};
+    case DELETE_PRODUCT_FAILURE:
+      return {...state,loading: false,error:action.error};
     default: return state
   }
 };
