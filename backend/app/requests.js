4f7835fefbb5f22ffe0bc16f7b9be4341e00f6ce
ramazan <assasins11122001@gmail.com>
2020-04-30 13:43:15 +0600

    Сделал route для получения списка заявок

--- backend/app/requests.js
+++ backend/app/requests.js
@@ -6,6 +6,21 @@ const permit = require('../middleware/permit');
 
 const router = express.Router();
 
+router.get('/', auth, async (req, res) => {
+	try {
+		if(req.currentUser.role === 'market') {
+			const requests = await Request.find({user: req.currentUser._id}).populate('user');
+
+			return res.send(requests);
+		}
+		const requests = await Request.find().populate('user');
+
+		return res.send(requests);
+	} catch (e) {
+		res.status(400).send(e);
+	}
+});
+
 router.post('/', [auth, permit('market')], async (req, res) => {
 	try {
 		const requests = req.body;
