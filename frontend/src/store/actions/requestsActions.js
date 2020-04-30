ee8a40da6dcbbbaee96d2e8bdae74391c5d03629
ramazan <assasins11122001@gmail.com>
2020-04-30 13:43:35 +0600

    Создал actions для получения списка заявок

--- frontend/src/store/actions/requestsActions.js
+++ frontend/src/store/actions/requestsActions.js
@@ -7,24 +7,41 @@ export const CREATE_REQUEST_INIT = 'CREATE_REQUEST_INIT';
 export const CREATE_REQUEST_SUCCESS = 'CREATE_REQUEST_SUCCESS';
 export const CREATE_REQUEST_ERROR = 'CREATE_REQUEST_ERROR';
 
+export const GET_REQUESTS_REQUEST = 'GET_REQUESTS_REQUEST';
+export const GET_REQUESTS_SUCCESS = 'GET_REQUESTS_SUCCESS';
+export const GET_REQUESTS_ERROR = 'GET_REQUESTS_ERROR';
+
 export const createRequestInit = () => ({type: CREATE_REQUEST_INIT});
 export const createRequestSuccess = () => ({type: CREATE_REQUEST_SUCCESS});
 export const createRequestError = error => ({type: CREATE_REQUEST_ERROR, error});
 
-export const createRequest = requestData => {
-	return async (dispatch) => {
-		try {
-			await axiosApi.post('/requests', requestData);
+export const getRequestsRequest = () => ({type: GET_REQUESTS_REQUEST});
+export const getRequestsSuccess = requests => ({type: GET_REQUESTS_SUCCESS, requests});
+export const getRequestsError = error => ({type: GET_REQUESTS_ERROR, error});
 
-			dispatch(createRequestSuccess());
-			notification.addNotification({
-				title: 'Создание заявки',
-				message: `Заявка создана успешно`,
-				...config.notification
-			});
-			dispatch(push('/'));
-		} catch (e) {
-			dispatch(createRequestError(e));
-		}
-	}
+export const getRequests = () => async (dispatch) => {
+    try {
+        dispatch(getRequestsRequest());
+        const requests = await axiosApi.get('/requests');
+
+        dispatch(getRequestsSuccess(requests.data));
+    } catch (e) {
+        dispatch(getRequestsError(e));
+    }
+};
+
+export const createRequest = requestData => async (dispatch) => {
+    try {
+        await axiosApi.post('/requests', requestData);
+
+        dispatch(createRequestSuccess());
+        notification.addNotification({
+            title: 'Создание заявки',
+            message: `Заявка создана успешно`,
+            ...config.notification
+        });
+        dispatch(push('/'));
+    } catch (e) {
+        dispatch(createRequestError(e));
+    }
 };
