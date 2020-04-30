1b45ed8be5fab6c0aa5ac6c1189f59a597521eb7
ramazan <assasins11122001@gmail.com>
2020-04-30 13:44:06 +0600

    Сделал обработку в reduce ля actions получения списка заявок

--- frontend/src/store/reducer/requestsReducer.js
+++ frontend/src/store/reducer/requestsReducer.js
@@ -1,11 +1,25 @@
-import {CREATE_REQUEST_ERROR, CREATE_REQUEST_INIT} from "../actions/requestsActions";
+import {
+	CREATE_REQUEST_ERROR,
+	CREATE_REQUEST_INIT,
+	GET_REQUESTS_ERROR,
+	GET_REQUESTS_REQUEST,
+	GET_REQUESTS_SUCCESS
+} from "../actions/requestsActions";
 
 const initialState = {
+	loading: null,
 	error: null,
+	list: []
 };
 
 const requestsReducer = (state = initialState, action) => {
 	switch (action.type) {
+		case GET_REQUESTS_REQUEST:
+			return {...state, loading: true};
+		case GET_REQUESTS_SUCCESS:
+			return {...state, list: action.requests, loading: false};
+		case GET_REQUESTS_ERROR:
+			return {...state, loading: false, error: action.error};
 		case CREATE_REQUEST_INIT:
 			return {...state, error: null};
 		case CREATE_REQUEST_ERROR:
