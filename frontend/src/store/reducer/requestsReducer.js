83ae2391472d04778793bf46dbf6b1c49079ce9a
ramazan <assasins11122001@gmail.com>
2020-04-30 15:26:04 +0600

    Сделал обработку actions для получения информации об одной заявке

--- frontend/src/store/reducer/requestsReducer.js
+++ frontend/src/store/reducer/requestsReducer.js
@@ -1,6 +1,6 @@
 import {
 	CREATE_REQUEST_ERROR,
-	CREATE_REQUEST_INIT,
+	CREATE_REQUEST_INIT, GET_REQUEST_ERROR, GET_REQUEST_REQUEST, GET_REQUEST_SUCCESS,
 	GET_REQUESTS_ERROR,
 	GET_REQUESTS_REQUEST,
 	GET_REQUESTS_SUCCESS
@@ -9,17 +9,27 @@ import {
 const initialState = {
 	loading: null,
 	error: null,
-	list: []
+	list: [],
+	request: {}
 };
 
 const requestsReducer = (state = initialState, action) => {
 	switch (action.type) {
+		case GET_REQUEST_REQUEST:
+			return {...state, loading: true};
+		case GET_REQUEST_SUCCESS:
+			return {...state, request: action.request, loading: false};
+		case GET_REQUEST_ERROR:
+			return {...state, loading: false, error: action.error};
+
 		case GET_REQUESTS_REQUEST:
 			return {...state, loading: true};
 		case GET_REQUESTS_SUCCESS:
+			console.log(action.requests);
 			return {...state, list: action.requests, loading: false};
 		case GET_REQUESTS_ERROR:
 			return {...state, loading: false, error: action.error};
+
 		case CREATE_REQUEST_INIT:
 			return {...state, error: null};
 		case CREATE_REQUEST_ERROR:
