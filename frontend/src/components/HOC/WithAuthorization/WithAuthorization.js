c496aa3605cb4c5844f97301ef23d6c099090dcd
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-05-01 15:28:03 +0600

    Использовал баг в HOC-е

--- frontend/src/components/HOC/WithAuthorization/WithAuthorization.js
+++ frontend/src/components/HOC/WithAuthorization/WithAuthorization.js
@@ -1,19 +1,21 @@
+import React from "react";
 import Login from "../../../containers/Login/Login";
 import AccessDenied from "../../AccessDenied/AccessDenied";
 
-const WithAuthorization = (WrappedComponent, roles) => {
-  roles = roles ? roles.split(' ') : []
-  console.log(roles);
+const WithAuthorization = (WrappedComponent, roles) => props => {
+  console.log(typeof roles);
+  const permissions = roles ? roles.split(' ') : []
+  console.log(permissions);
   const user = JSON.parse(localStorage.getItem('state'));
 
-  let component = WrappedComponent;
+  let component = <WrappedComponent {...props}/>;
 
   if (!user.users.user) {
-    component = Login
+    component = <Login {...props}/>
   }
 
-  if(user.users.user && !user.users.user.role.includes(roles) && roles.length !== 0) {
-    component = AccessDenied
+  if(user.users.user && !user.users.user.role.includes(permissions) && permissions.length !== 0) {
+    component = <AccessDenied {...props}/>
   }
 
   return component;
