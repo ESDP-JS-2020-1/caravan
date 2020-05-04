a4c84b126c1a7a491a54cb2a300c376deb0b22ea
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-05-04 13:23:43 +0600

    Довил новое свойство в модели продукта #39 Добавил свойство isRefrigeratorRequired для отражения обязательности холодильника со стороны сервера

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,19 +1,23 @@
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  amount: {
-    type: String,
-    required: true
-  },
-  price: {
-    type: String,
-    required: true
-  },
-  image: String
+    name: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: String,
+        required: true
+    },
+    isRefrigeratorRequired: {
+      type: Boolean,
+      default: false
+    },
+    image: String
 });
 
 const Product = mongoose.model('Product', ProductSchema);
