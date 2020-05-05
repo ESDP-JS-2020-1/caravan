46f03c575a5b98096dcc5c23f4c0f068248ad080
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-05-05 14:11:57 +0600

    Добавил переключатель для выбора языка #48

--- frontend/src/components/UI/toolBar/toolBar.js
+++ frontend/src/components/UI/toolBar/toolBar.js
@@ -1,13 +1,71 @@
 import React from 'react';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import AuthUserToolBar from "./authUserToolBar";
 import NoAuthUserToolBar from "./noAuthToolBar";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import Box from "@material-ui/core/Box";
+import TranslateIcon from '@material-ui/icons/Translate';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import {setLanguage} from "../../../store/actions/languageActions";
+
+const useStyles = makeStyles({
+    button: {
+        color: 'white'
+    }
+});
 
 const ToolBar = () => {
+
+    const classes = useStyles();
+
     const user = useSelector(state => state.users.user);
+    const language = useSelector(state => state.language.name);
+
+    const dispatch = useDispatch()
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const switchLanguage = name => {
+        dispatch(setLanguage(name))
+        handleClose()
+    }
 
     return (
         <>
+            <Box mr={3}>
+                <Button
+                    startIcon={<TranslateIcon/>}
+                    endIcon={<ExpandMoreIcon/>}
+                    color='primary'
+                    aria-controls="language-switcher"
+                    aria-haspopup="true"
+                    onClick={handleClick}
+                    className={classes.button}
+                >
+                    {language}
+                </Button>
+                <Menu
+                    id="language-switcher"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                >
+                    <MenuItem onClick={() => switchLanguage('ru')}>Русский</MenuItem>
+                    <MenuItem onClick={() => switchLanguage('kg')}>Кыргызча</MenuItem>
+                </Menu>
+            </Box>
             {user && <AuthUserToolBar/>}
             {!user && <NoAuthUserToolBar/>}
         </>
