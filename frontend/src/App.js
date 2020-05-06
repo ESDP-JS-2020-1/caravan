c3b0fc462ab73473a814d365d75b29459f3d03f6
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-05-06 14:32:37 +0600

    Переписал роутинг с использованием компонента PrivateRoute #53

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ import RequestList from "./containers/RequestList/RequestList";
 import RequestInfo from "./containers/RequestInfo/RequestInfo";
 import EditRequest from "./containers/EditRequest/EditRequest";
 import UserInfo from "./containers/UserInfo/UserInfo";
+import PrivateRoute from "./components/HOC/PrivateRoute/PrivateRoute";
 
 
 
@@ -29,19 +30,19 @@ function App() {
             <Container>
                 <Switch>
                     <Route path="/login" exact component={Login}/>
-                    <Route path="/users" exact component={UsersList}/>
-                    <Route path="/user/:id" exact component={UserInfo}/>
-                    <Route path="/users/new" exact component={AddUser}/>
-                    <Route path="/users/edit/:id" exact component={EditUser}/>
-                    <Route path="/users/:id" exact component={UsersList}/>
-                    <Route path="/products" exact component={ProductList}/>
-                    <Route path="/product/add" exact component={AddProduct}/>
-                    <Route path="/product/edit/:id" exact component={EditProduct}/>
-                    <Route path="/history" exact component={Histories}/>
-                    <Route path="/requests" exact component={RequestList}/>
-                    <Route path="/requests/new" exact component={AddNewRequest}/>
-                    <Route path="/requests/:id" exact component={RequestInfo}/>
-                    <Route path="/requests/edit/:id" exact component={EditRequest}/>
+                    <PrivateRoute path="/users" roles={['admin', 'operator']} exact component={UsersList}/>
+                    <PrivateRoute path="/user/:id" roles={['admin', 'operator']} exact component={UserInfo}/>
+                    <PrivateRoute path="/users/new" roles={['admin']} exact component={AddUser}/>
+                    <PrivateRoute path="/users/edit/:id" roles={['admin']} exact component={EditUser}/>
+                    <PrivateRoute path="/users/:id" exact roles={['admin', 'operator']} component={UsersList}/>
+                    <PrivateRoute path="/products" exact component={ProductList}/>
+                    <PrivateRoute path="/product/add" exact roles={['admin']} component={AddProduct}/>
+                    <PrivateRoute path="/product/edit/:id" exact roles={['admin']} component={EditProduct}/>
+                    <PrivateRoute path="/history" exact roles={['admin']} component={Histories}/>
+                    <PrivateRoute path="/requests" exact component={RequestList}/>
+                    <PrivateRoute path="/requests/new" roles={['admin', 'market']} exact component={AddNewRequest}/>
+                    <PrivateRoute path="/requests/:id" exact component={RequestInfo}/>
+                    <PrivateRoute path="/requests/edit/:id" exact roles={['admin']} component={EditRequest}/>
                 </Switch>
             </Container>
         </div>
