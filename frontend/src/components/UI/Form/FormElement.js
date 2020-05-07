4e268a6398785cb33073c924c268d5db20a70ae1
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-05-07 18:34:57 +0600

    Реализовал тип "search" для FormElement.js #65

--- frontend/src/components/UI/Form/FormElement.js
+++ frontend/src/components/UI/Form/FormElement.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {makeStyles} from '@material-ui/core/styles';
+import {fade, makeStyles} from '@material-ui/core/styles';
 import TextField from "@material-ui/core/TextField";
 import PhotoCameraIcon from '@material-ui/icons/PhotoCamera';
 import Button from "@material-ui/core/Button";
@@ -10,8 +10,10 @@ import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import InputLabel from "@material-ui/core/InputLabel";
 import Checkbox from "@material-ui/core/Checkbox";
+import SearchIcon from "@material-ui/icons/Search";
+import InputBase from "@material-ui/core/InputBase";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
     root: {
         width: '100%',
         marginBottom: '2%',
@@ -21,8 +23,45 @@ const useStyles = makeStyles(() => ({
     },
     select: {
         width: '100%'
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(3),
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '20ch',
+        }
     }
 }));
+
 const FormElement = props => {
     const classes = useStyles();
 
@@ -41,6 +80,25 @@ const FormElement = props => {
         autoComplete={props.autoComplete}
         placeholder={props.placeholder}
     />;
+
+    if (props.type === 'search') {
+        field = <div className={classes.search}>
+            <div className={classes.searchIcon}>
+                <SearchIcon />
+            </div>
+            <InputBase
+                placeholder={props.title}
+                value={props.value}
+                onChange={props.onChange}
+                name={props.propertyName}
+                classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                }}
+                inputProps={{ 'aria-label': 'search' }}
+            />
+        </div>
+    }
 
     if (props.type === 'checkbox') {
         field = (
