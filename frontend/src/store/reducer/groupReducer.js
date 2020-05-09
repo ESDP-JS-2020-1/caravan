51aacf963499472b5f4eb70e9a7cd75359a9c4ac
ramazan <assasins11122001@gmail.com>
2020-05-10 00:08:36 +0600

    Сделал свойства для одной группы и списка групп

--- frontend/src/store/reducer/groupReducer.js
+++ frontend/src/store/reducer/groupReducer.js
@@ -1,9 +1,32 @@
+import {
+    GET_GROUP_ERROR, GET_GROUP_REQUEST, GET_GROUP_SUCCESS,
+    GET_GROUPS_ERROR, GET_GROUPS_REQUEST, GET_GROUPS_SUCCESS,
+} from "../actions/actionsTypes";
+
 const initialState = {
-    groups: []
+    loading: false,
+    error: null,
+    groups: [],
+    group: {}
 };
 
 const groupReducer = (state = initialState, action) => {
-    return state
+    switch (action.type) {
+        case GET_GROUPS_REQUEST:
+            return {...state, loading: true};
+        case GET_GROUPS_SUCCESS:
+            return {...state, groups: action.groups, loading: false};
+        case GET_GROUPS_ERROR:
+            return {...state, error: action.error, loading: false};
+
+        case GET_GROUP_REQUEST:
+            return {...state, loading: true};
+        case GET_GROUP_SUCCESS:
+            return {...state, group: action.group, loading: false};
+        case GET_GROUP_ERROR:
+            return {...state, error: action.error, loading: false};
+        default: return state
+    }
 };
 
 export default groupReducer;
