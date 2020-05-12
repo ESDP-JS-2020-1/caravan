0158e920e09ffa28782973994ed65a9175d424b0
ramazan <assasins11122001@gmail.com>
2020-05-12 12:32:48 +0600

    Сделал вывод всех пермитов пользователя из функции isAuth

--- backend/middleware/isAuth.js
+++ backend/middleware/isAuth.js
@@ -1,16 +1,22 @@
 const User = require('../models/User');
+const Group = require('../models/Group');
 
 module.exports = async (req, res, next) => {
     const authHeader = req.get('Authorization');
-    if(!authHeader) {
+    if (!authHeader) {
         return res.status(404).send({error: 'Access denied'})
     } else {
         const [type, token] = authHeader.split(' ');
-        const user = await User.findOne({ token });
-        if(type !== 'token' || !user) {
+        const user = await User.findOne({token});
+        const groups = await Group.find({list: {$elemMatch: {user: user._id}}});
+        let permissions = new Set();
+        groups.forEach(elemt => elemt.permissions.forEach(permit => permissions.add(permit)));
+        permissions = [...permissions];
+        if (type !== 'token' || !user) {
             return res.status(404).send({error: 'Access denied'})
         } else {
             req.currentUser = user;
+            req.currentPermissions = permissions;
             return next();
         }
     }
