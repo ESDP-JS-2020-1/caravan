10d705323f8a7d627a7f6fca566e3b0e83f2624c
ramazan <assasins11122001@gmail.com>
2020-05-17 13:43:11 +0600

    Сделал вывод разрешения которое нужно для доступа к функции если этого разрешения нет у пользователя

--- backend/middleware/permit.js
+++ backend/middleware/permit.js
@@ -1,7 +1,7 @@
-const permit = (permit) => (req, res, next) => {
+const permit = permit => (req, res, next) => {
     if(!req.currentUser) return res.status(401).send({error: 'User is unauthenticated'});
 
-    if(!req.currentPermissions.includes(permit)) return res.status(403).send({error: 'User is unauthenticated'});
+    if(!req.currentPermissions.includes(permit)) return res.status(403).send({error: `You dont have "${permit}" permission!`});
 
     next();
 };
