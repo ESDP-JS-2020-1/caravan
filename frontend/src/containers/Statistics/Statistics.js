0f1314c11625fb325a4d36849936af00fb53f5e3
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-05-19 14:53:19 +0600

    Реализовал компонент для вывода статистики Statistics.js #96

--- frontend/src/containers/Statistics/Statistics.js
+++ frontend/src/containers/Statistics/Statistics.js
@@ -1,25 +1,135 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+
 import {useDispatch, useSelector} from "react-redux";
+import {useParams} from 'react-router-dom'
+
+import {Bar} from 'react-chartjs-2';
+import moment from "moment";
+
 import {getStatistics} from "../../store/actions/statisticsActions";
 import {getProductsList} from "../../store/actions/productsActions";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import {wordList} from "../../wordList";
+import FormElement from "../../components/UI/Form/FormElement";
+import Button from "@material-ui/core/Button";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import Box from "@material-ui/core/Box";
+
+
+const useStyle = makeStyles({
+    search: {
+        marginLeft: 'auto'
+    }
+})
+
 
 const Statistics = () => {
+
+    const classes = useStyle()
+
+    const [numberOfDays, setNumberOfDays] = useState('7');
+
     const dispatch = useDispatch();
+    const {id} = useParams()
+
     const statistics = useSelector(state => state.statistics.statistics);
-    console.log(statistics);
+    const language = useSelector(state => state.language.name);
+
+    const inputChangeHandler = e => setNumberOfDays(e.target.value)
+    const search = () => {
+        if(numberOfDays) {
+            dispatch(getStatistics(id, numberOfDays));
+        }
+    }
 
     useEffect(() => {
         dispatch(getProductsList());
-    }, [dispatch]);
+        dispatch(getStatistics(id, numberOfDays));
+    }, [dispatch, numberOfDays, id]);
 
-    const clickHandler = () => {
-       dispatch(getStatistics('5ec24b5c3c7485425bc5b8c1', '5'));
-    };
+    const dateFormat = 'MMMM Do YYYY, h:mm:ss a';
+
+    const amounts = statistics.map(elem => parseInt(elem.amount))
+    const dates = statistics.map(elem => moment(elem.date).format(dateFormat))
+
+    const data = {
+        labels: dates,
+        datasets: [{
+            label: '# of Votes',
+            data: amounts,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    }
 
     return (
-        <div>
-            <button onClick={clickHandler}>Поиск</button>
-        </div>
+        <Box mt={1}>
+            <Grid container direction='column' spacing={1}>
+                <Grid container alignItems='center'>
+                    <Grid item>
+                        <Typography variant='h2'>
+                            {wordList[language].statistic.statisticTitle}
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={6} className={classes.search}>
+                        <Grid container alignItems='center' spacing={1}>
+                            <Grid item xs>
+                                <FormElement
+                                    type='number'
+                                    propertyName='numberOfDays'
+                                    title={`${wordList[language].statistic.statisticSearchFieldTitle}...`}
+                                    onChange={inputChangeHandler}
+                                    value={numberOfDays}
+                                />
+                            </Grid>
+                            <Grid item>
+                                <Button variant='contained' color='primary' onClick={search}>
+                                    {wordList[language].search}
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item>
+                    <Typography variant='h6'>
+                        {wordList[language].statistic.salesStatistics}
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <Bar
+                        data={data}
+                        width={100}
+                        height={500}
+                        options={{
+                            maintainAspectRatio: false,
+                            scales: {
+                                yAxes: [{
+                                    ticks: {
+                                        beginAtZero: true
+                                    }
+                                }]
+                            }
+                        }}
+                    />
+                </Grid>
+            </Grid>
+        </Box>
     );
 };
 
