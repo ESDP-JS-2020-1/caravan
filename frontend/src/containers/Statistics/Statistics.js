3b180bba454eb42784ad0e87711049d2515ca06a
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-05-19 20:24:58 +0600

    Добавил генерацию рандомных цветов для колонок статистики

--- frontend/src/containers/Statistics/Statistics.js
+++ frontend/src/containers/Statistics/Statistics.js
@@ -28,6 +28,16 @@ const Statistics = () => {
 
     const classes = useStyle()
 
+    const createRandomColor = () => {
+        const r = Math.floor(Math.random() * 255);
+        const g = Math.floor(Math.random() * 255);
+        const b = Math.floor(Math.random() * 255);
+
+        const opacityFactor = 0.7
+
+        return `rgba(${r},${g},${b},${opacityFactor})`
+    }
+
     const [numberOfDays, setNumberOfDays] = useState('7');
 
     const dispatch = useDispatch();
@@ -52,28 +62,14 @@ const Statistics = () => {
 
     const amounts = statistics.map(elem => parseInt(elem.amount))
     const dates = statistics.map(elem => moment(elem.date).format(dateFormat))
+    const colors = statistics.map(() => createRandomColor())
 
     const data = {
         labels: dates,
         datasets: [{
             label: '# of Votes',
             data: amounts,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
+            backgroundColor: colors,
             borderWidth: 1
         }]
     }
