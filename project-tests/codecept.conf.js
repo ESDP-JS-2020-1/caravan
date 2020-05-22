ed199266445a13c8aa1252d88cf48e6f288133d4
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-05-22 13:58:25 +0600

    Добавил проверку на наличие переменной окружения CI #90

--- project-tests/codecept.conf.js
+++ project-tests/codecept.conf.js
@@ -3,7 +3,8 @@ exports.config = {
   helpers: {
     Puppeteer: {
       url: 'http://localhost:3010',
-      show: true,
+      show: !process.env.CI,
+      headless: !!!process.env.CI,
       windowSize: '800x600'
     }
   },
