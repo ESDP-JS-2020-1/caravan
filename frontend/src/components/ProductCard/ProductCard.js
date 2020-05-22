5e2eb128b6feb903b6ebdfa50ad27f18e7a09678
ramazan <assasins11122001@gmail.com>
2020-05-22 14:24:32 +0600

    Улучшил отображение карточек с продуктами

--- frontend/src/components/ProductCard/ProductCard.js
+++ frontend/src/components/ProductCard/ProductCard.js
@@ -1,34 +1,27 @@
 import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import Box from "@material-ui/core/Box";
 import apiURL from "../../apiURL";
 import {useSelector} from "react-redux";
 import {NavLink} from "react-router-dom";
 import {wordList} from "../../wordList";
 import {checkPermission} from "../../CheckPermission";
 import Grid from "@material-ui/core/Grid";
+import EqualizerIcon from "@material-ui/icons/Equalizer";
+import EditIcon from "@material-ui/icons/Edit";
+import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles({
-    root: {width: '100%'},
+    root: {width: '100%', padding: '10px'},
     media: {
         height: 140,
     },
-    typography:{
+    typography: {
         fontSize: 14
-    },
-    cardAction:{
-        display:'flex',
-        flexDirection:'column',
-    },
-    contentText:{
-        textAlign:'center'
     },
     padding: {
         padding: '10px'
@@ -41,53 +34,59 @@ const ProductCard = (props) => {
     const classes = useStyles();
     const language = useSelector(state => state.language.name);
     return (
-<Grid item xs={12} sm={6} md={4} className={classes.padding}>
-        <Card  className={classes.root}>
-            <CardActionArea>
-                {props.image && <CardMedia
-                    className={classes.media}
-                    image={apiURL.url + '/uploads/productImage/' + props.image}
-                    title={props.title}
+        <Grid item xs={12} sm={6} md={4} className={classes.padding}>
+            <Paper elevation={3}>
+                <Card className={classes.root}>
+                    {props.image && <CardMedia
+                        className={classes.media}
+                        image={apiURL.url + '/uploads/productImage/' + props.image}
+                        title={props.title}
 
-                />}
-                <CardContent className={classes.contentText}>
-                    <Typography className={classes.typography} gutterBottom variant="h5" component="h5">Название: {props.title}</Typography>
-                    <Typography className={classes.typography}  gutterBottom variant="h5" component="h5">Кол-во: {props.amount}</Typography>
-                    <Typography className={classes.typography}  gutterBottom variant="h5" component="h5">Тип: {props.productType}</Typography>
-                    <Typography className={classes.typography}  gutterBottom variant="h5" component="h5">Цена: {props.price}</Typography>
-                    <Typography className={classes.typography}  gutterBottom variant="h5" component="h5">Холодильник:
-                        {props.isRefrigeratorRequired ?
-                            wordList[language].necessarily :
-                            wordList[language].notNecessary
-                        }
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-            <CardActions  className={classes.cardAction}>
-                <Box>
-                {checkPermission('editProduct')
-                && <Button xs={12} sm={12}  color="primary"
-                           component={NavLink}
-                           to={`/product/edit/${props.id}`}
-                           id={props.edit}
+                    />}
+                    <CardContent >
+                        <Typography className={classes.typography} gutterBottom variant="h5"
+                                    component="h5"><b>Название: </b> {props.title}</Typography>
+                        <Typography className={classes.typography} gutterBottom variant="h5"
+                                    component="h5"><b>Кол-во: </b> {props.amount}</Typography>
+                        <Typography className={classes.typography} gutterBottom variant="h5"
+                                    component="h5"><b>Тип: </b> {props.productType}</Typography>
+                        <Typography className={classes.typography} gutterBottom variant="h5"
+                                    component="h5"><b>Цена: </b> {props.price}</Typography>
+                        <Typography className={classes.typography} gutterBottom variant="h5" component="h5">
+                            <b>Холодильник: </b>
+                            {props.isRefrigeratorRequired ?
+                                wordList[language].necessarily :
+                                wordList[language].notNecessary
+                            }
+                        </Typography>
 
-                >
-                    Редактировать
-                </Button>}
-                </Box>
-                <Box>
-                {checkPermission('getStatistic')
-                && <Button xs={12} sm={12}  color="primary"
-                           component={NavLink}
-                           to={`/product/stat/${props.id}`}
+                        {checkPermission('editProduct')
+                        && <Button xs={12} sm={12}
+                                   variant='contained'
+                                   color="primary"
+                                   component={NavLink}
+                                   to={`/product/edit/${props.id}`}
+                                   id={props.edit}
+                                   startIcon={<EditIcon/>}
+                                   style={{margin: '10px 0 7px 0'}}
+                        >
+                            Редактировать
+                        </Button>}
+                        {checkPermission('getStatistic')
+                        && <Button xs={12} sm={12}
+                                   variant='contained'
+                                   color="primary"
+                                   component={NavLink}
+                                   to={`/product/stat/${props.id}/7`}
+                                   startIcon={<EqualizerIcon/>}
+                        >
+                            Смотреть статистику
+                        </Button>}
+                    </CardContent>
 
-                >
-                    Смотреть статистику
-                </Button>}
-                    </Box>
-            </CardActions>
-        </Card>
-</Grid>
+                </Card>
+            </Paper>
+        </Grid>
     );
 }
 export default ProductCard;
