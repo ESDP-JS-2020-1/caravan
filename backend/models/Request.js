5eaf90721b547f1833ade164cba4604e2e41f306
ramazan <assasins11122001@gmail.com>
2020-05-22 13:14:31 +0600

    Убрал ненужные console.log()

--- backend/models/Request.js
+++ backend/models/Request.js
@@ -31,14 +31,6 @@ const RequestSchema = new Schema({
     }
 });
 
-RequestSchema.virtual('type').get(function () {
-console.log(this)
-})
-
-RequestSchema.pre('validate', function (next) {
-    console.log(this )
-})
-
 const Request = mongoose.model('Request', RequestSchema);
 
 module.exports = Request;
