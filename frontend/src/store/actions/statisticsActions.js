229c4e62670ec4fa758b7c79257a0b82566583d0
ramazan <assasins11122001@gmail.com>
2020-05-23 21:22:34 +0600

    Сделал action для получения статистики о пользователе

--- frontend/src/store/actions/statisticsActions.js
+++ frontend/src/store/actions/statisticsActions.js
@@ -7,13 +7,25 @@ export const getStatisticRequest = () => ({type: GET_STATISTIC_REQUEST});
 export const getStatisticSuccess = (statistics) => ({type: GET_STATISTIC_SUCCESS, statistics});
 export const getStatisticError = (error) => ({type: GET_STATISTIC_ERROR, error});
 
-export const getStatistics = (productId, day) => async dispatch => {
+export const getStatisticsProduct = (productId, day) => async dispatch => {
     try {
         dispatch(getStatisticRequest());
-        const response = await axiosApi.get(`/stat/${productId}/${day}`);
+        const response = await axiosApi.get(`/stat/product/${productId}/${day}`);
         dispatch(getStatisticSuccess(response.data));
 
         dispatch(push(`/product/stat/${productId}/${day}`))
+    } catch (e) {
+        dispatch(getStatisticError(e))
+    }
+};
+
+export const getStatisticsUser = (userId, day) => async dispatch => {
+    try {
+        dispatch(getStatisticRequest());
+        const response = await axiosApi.get(`/stat/user/${userId}/${day}`);
+        dispatch(getStatisticSuccess(response.data));
+
+        dispatch(push(`/users/stat/${userId}/${day}`))
     } catch (e) {
         dispatch(getStatisticError(e))
     }
