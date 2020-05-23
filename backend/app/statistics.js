ab7a054fbf94de7b44ad008679206243c1eb9c5b
ramazan <assasins11122001@gmail.com>
2020-05-23 21:21:44 +0600

    Сделал роут для получения статистики о пользователе

--- backend/app/statistics.js
+++ backend/app/statistics.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
 const Statistics = require('../models/Statistic'),
-      Product = require('../models/Product');
+      Product = require('../models/Product'),
+      User = require('../models/User');
 const isAuth = require('../middleware/isAuth');
 
 const router = express.Router();
 
-router.get('/:id/:date', isAuth, async (req, res) => {
+router.get('/product/:id/:date', isAuth, async (req, res) => {
     try {
         const statistic = await Statistics.find({
             product: req.params.id,
@@ -16,6 +17,22 @@ router.get('/:id/:date', isAuth, async (req, res) => {
         const product = await Product.findOne({_id: req.params.id});
 
         res.send({product, statistic})
+    } catch (e) {
+        console.log(e);
+        res.status(404).send(e)
+    }
+});
+
+router.get('/user/:id/:date', isAuth, async (req, res) => {
+    try {
+        const statistic = await Statistics.find({
+            user: req.params.id,
+            date: {$gte: new Date(new Date() - req.params.date * 60 * 60 * 24 * 1000)}
+        }).populate(['user', 'product']);
+
+        const user = await User.findOne({_id: req.params.id});
+
+        res.send({user, statistic})
     } catch (e) {
         console.log(e);
         res.status(404).send(e)
