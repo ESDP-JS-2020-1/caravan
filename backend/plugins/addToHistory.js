359c7e2b5208253086f21d9688d002fde7422a35
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-05-26 16:55:58 +0600

    Реализовал добавление в историю после сохронения документа #58

--- backend/plugins/addToHistory.js
+++ backend/plugins/addToHistory.js
@@ -1,3 +1,4 @@
+const History = require('../models/History');
 
 module.exports = function (schema) {
 
@@ -6,9 +7,14 @@ module.exports = function (schema) {
     schema.pre('save', function (next, request) {
         user = request.currentUser;
 
-        type = this.isNew ? 'add' : 'edit'
-        if(this.isRemoved) type = 'delete';
+        type = this.isNew ? 'add' : 'edit';
+        if(request.isRemoved) type = 'delete';
 
         next()
     })
+
+    schema.pre('save', async function (info) {
+        await History.create({ user, info, type })
+    })
+
 }
