9a5c205ed8fbc4abba1278d0770e45032becff94
Ulan Beltaev <ulanbeltaev@gmail.com>
2020-05-26 16:20:04 +0600

    Немного увеличил высоту картинки

--- frontend/src/components/ProductCard/ProductCard.js
+++ frontend/src/components/ProductCard/ProductCard.js
@@ -3,7 +3,6 @@ import {makeStyles} from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import apiURL from "../../apiURL";
 import {useSelector} from "react-redux";
@@ -18,7 +17,7 @@ import IconButton from "@material-ui/core/IconButton";
 const useStyles = makeStyles({
     root: {width: '100%', padding: '10px'},
     media: {
-        height: 140,
+        height: 200,
     },
     typography: {
         fontSize: 14
