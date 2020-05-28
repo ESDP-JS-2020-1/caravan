1685ef8aed25035c3aacf1121ae16e3d4d80763d
ramazan <assasins11122001@gmail.com>
2020-05-28 11:50:59 +0600

    Сделал файл со списком всех доступных разрешений на странийе

---
+++ backend/permissionsForUsers.js
@@ -0,0 +1,24 @@
+module.exports = [
+      'getUser'
+    , 'addUser'
+    , 'deleteUser'
+    , 'editUser'
+
+    , 'addProduct'
+    , 'deleteProduct'
+    , 'editProduct'
+
+    , 'getGroup'
+    , 'editGroup'
+    , 'addGroup'
+    , 'deleteGroup'
+
+    , 'getRequest'
+    , 'addRequest'
+    , 'deleteRequest'
+    , 'editRequest'
+    , 'closeRequest'
+
+    , 'viewHistory'
+    , 'getStatistic'
+];
