fa889a59b05556f67a9e243e6767345679badf01
ramazan <assasins11122001@gmail.com>
2020-05-28 13:55:07 +0600

    Сделал обработку экшена для получения массива разрешений #113

--- frontend/src/store/reducer/groupReducer.js
+++ frontend/src/store/reducer/groupReducer.js
@@ -1,23 +1,28 @@
 import {
     GET_GROUP_ERROR, GET_GROUP_REQUEST, GET_GROUP_SUCCESS,
-    GET_GROUPS_ERROR, GET_GROUPS_REQUEST, GET_GROUPS_SUCCESS,
+    GET_GROUPS_ERROR, GET_GROUPS_REQUEST, GET_GROUPS_SUCCESS, GET_PERMISSIONS_SUCCESS,
 } from "../actions/actionsTypes";
 
 const initialState = {
     loading: false,
     error: null,
     groups: [],
-    group: {}
+    group: {},
+    permissions: undefined
 };
 
 const groupReducer = (state = initialState, action) => {
     switch (action.type) {
+
         case GET_GROUPS_REQUEST:
             return {...state, loading: true};
         case GET_GROUPS_SUCCESS:
             return {...state, groups: action.groups, loading: false};
         case GET_GROUPS_ERROR:
             return {...state, error: action.error, loading: false};
+
+        case GET_PERMISSIONS_SUCCESS:
+            return {...state, permissions: action.permission};
 
         case GET_GROUP_REQUEST:
             return {...state, loading: true};
