8e0578ea5cc61a0e220712e2a39e02ca96a8ca82
spartak <fomenkospartak@gmail.com>
2020-05-30 21:26:34 +0600

    выполнил тикет #117

--- frontend/src/CheckPermission.js
+++ frontend/src/CheckPermission.js
@@ -2,5 +2,5 @@ import {store} from "./store/configureStore";
 
 export const checkPermission = (permit) => {
     const user = store.getState().users.user;
-    return user && user.permissions.includes(permit)
+    return user && user.permissions && user.permissions.includes(permit)
 };
