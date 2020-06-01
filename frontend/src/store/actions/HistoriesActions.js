599dbf8a8f9082bf1585a70e6528ab1c76a0b654
ramazan <assasins11122001@gmail.com>
2020-06-01 13:51:39 +0600

    Переделал получение истории #127

--- frontend/src/store/actions/HistoriesActions.js
+++ frontend/src/store/actions/HistoriesActions.js
@@ -7,10 +7,10 @@ export const geHistoriesRequest = () => ({type: GET_HISTORIES_REQUEST});
 export const getHistoriesSuccess = histories => ({type: GET_HISTORIES_SUCCESS, histories});
 export const getHistoriesError = error => ({type: GET_HISTORIES_ERROR, error});
 
-export const getHistoriesList = () => async dispatch => {
+export const getHistoriesList = (page, limit) => async dispatch => {
     try {
         dispatch(geHistoriesRequest());
-        const histories = await axiosApi.get('/histories');
+        const histories = await axiosApi.get(`/histories/ ${page} / ${limit} `);
         dispatch(getHistoriesSuccess(histories.data))
     } catch (error) {
         dispatch(getHistoriesError(error));
