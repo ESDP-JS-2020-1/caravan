dc906959a18905ba0e0ab45acb4d8178d8c25e23
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-06-02 22:49:54 +0600

    Добавил сортировку по дате при запросе корзины #134

--- frontend/src/store/actions/trashActions.js
+++ frontend/src/store/actions/trashActions.js
@@ -5,20 +5,30 @@ export const getTrashRequest = () => ({type: GET_TRASH_REQUEST});
 export const getTrashSuccess = trash => ({type: GET_TRASH_SUCCESS, trash});
 export const getTrashFailure = error => ({type: GET_TRASH_FAILURE, error});
 
-export const getTrash = () => async dispatch => {
+export const getTrash = type => async dispatch => {
     try {
         const trash = [];
 
-        trash.push(axiosApi.get('/users/removed'))
-        trash.push(axiosApi.get('/requests/removed'))
-        trash.push(axiosApi.get('/products/removed'))
-        trash.push(axiosApi.get('/groups/removed'))
+        if (!type) {
+            trash.push(axiosApi.get('/users/removed'))
+            trash.push(axiosApi.get('/requests/removed'))
+            trash.push(axiosApi.get('/products/removed'))
+            trash.push(axiosApi.get('/groups/removed'))
+        } else {
+            trash.push(axiosApi.get(`/${type}/removed`))
+        }
 
         dispatch(getTrashRequest());
 
         const actualTrash = await Promise.all(trash);
 
-        const data = actualTrash.map(data => data.data).flat();
+        let data = actualTrash.map(data => data.data).flat();
+
+        data = data.sort((a, b) => {
+            const c = new Date(a.date);
+            const d = new Date(b.date);
+            return d-c
+        })
 
         dispatch(getTrashSuccess(data));
     } catch (e) {
