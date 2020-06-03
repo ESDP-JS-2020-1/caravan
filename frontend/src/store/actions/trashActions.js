4b37b6d6ef31bcd7eeb33b0b62eb3259755a22ed
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-06-03 12:05:09 +0600

    Добавил переадресацию на путь относительно выбраного типа #134

--- frontend/src/store/actions/trashActions.js
+++ frontend/src/store/actions/trashActions.js
@@ -1,5 +1,6 @@
 import axiosApi from "../../axiosAPI";
 import {GET_TRASH_FAILURE, GET_TRASH_REQUEST, GET_TRASH_SUCCESS} from "./actionsTypes";
+import {push} from 'connected-react-router'
 
 export const getTrashRequest = () => ({type: GET_TRASH_REQUEST});
 export const getTrashSuccess = trash => ({type: GET_TRASH_SUCCESS, trash});
@@ -9,12 +10,14 @@ export const getTrash = type => async dispatch => {
     try {
         const trash = [];
 
-        if (!type) {
+        if (type === 'all') {
+            dispatch(push(`/trash/${type}`))
             trash.push(axiosApi.get('/users/removed'))
             trash.push(axiosApi.get('/requests/removed'))
             trash.push(axiosApi.get('/products/removed'))
             trash.push(axiosApi.get('/groups/removed'))
         } else {
+            dispatch(push(`/trash/${type}`))
             trash.push(axiosApi.get(`/${type}/removed`))
         }
 
