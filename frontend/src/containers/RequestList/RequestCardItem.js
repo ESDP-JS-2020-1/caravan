8c68c81f85d2ff7fadb09e6bb714968b0e9968be
Ulan Beltaev <ulanbeltaev@gmail.com>
2020-06-03 14:59:03 +0600

    Добавил новый компонент для его отображения на мобильных устройствах

---
+++ frontend/src/containers/RequestList/RequestCardItem.js
@@ -0,0 +1,104 @@
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import { makeStyles } from '@material-ui/core/styles';
+import {getRequests} from "../../store/actions/requestsActions";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import CardActions from "@material-ui/core/CardActions";
+import IconButton from "@material-ui/core/IconButton";
+import {NavLink} from "react-router-dom";
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import EditIcon from "@material-ui/icons/Edit";
+import moment from "moment";
+import Chip from "@material-ui/core/Chip";
+import {wordList} from "../../wordList";
+import Grid from "@material-ui/core/Grid";
+
+
+const useStyles = makeStyles({
+	root: {
+		width: 350,
+	},
+	pos: {
+		marginBottom: 16,
+		fontSize: '18px'
+	},
+	status: {
+		fontSize: '18px',
+	},
+	chip: {
+		fontSize: '16px'
+	},
+	btn: {
+		justifyContent: 'space-around',
+		marginTop: 12,
+	},
+	padding: {
+		padding: '10px'
+	}
+});
+
+const RequestCardItem = props => {
+	const classes = useStyles();
+	const dispatch = useDispatch();
+
+	const requests = useSelector(state => state.requests.list);
+	const language = useSelector(state => state.language.name);
+
+
+	useEffect(() => {
+		dispatch(getRequests());
+	}, [dispatch]);
+
+		return (
+			<Grid container item className={classes.padding}>
+				{requests && requests.map(elem => (
+					<Card className={classes.root} key={elem._id}>
+						<CardContent>
+							<Typography variant="body2" component="p" className={classes.pos}>
+								<b>{wordList[language].requestList.tableUser}: </b>{elem.user.displayName}
+							</Typography>
+							<Typography variant="body2" component="p" className={classes.pos}>
+								<b>{wordList[language].requestList.tableDate}: </b>{moment(elem.date).format('MMMM Do YYYY, h:mm:ss a')}
+							</Typography>
+							<div className={classes.status}>
+								<b>{wordList[language].requestList.tableStatus}: </b>
+								{elem.status === 'pending' &&
+								<Chip label={wordList[language].requestListItem.label_1} className={classes.chip}/>}
+								{elem.status === 'performed' &&
+								<Chip color='primary' label={wordList[language].requestListItem.label_2} className={classes.chip}/>}
+								{elem.status === 'closed' &&
+								<Chip color='secondary' label={wordList[language].requestListItem.label_3} className={classes.chip}/>}
+							</div>
+							<CardActions className={classes.btn}>
+								{elem.status !== 'closed' &&
+								<IconButton
+									aria-label="edit"
+									component={NavLink}
+									to={`/requests/edit/${elem._id}`}
+									id={elem._id}
+									exact
+								>
+									<EditIcon/>
+								</IconButton>
+								}
+
+								<IconButton
+									aria-label="show info"
+									component={NavLink}
+									to={`/requests/${elem._id}`}
+									id={elem.user.role + 'Q' + elem.user.phone}
+									exact
+								>
+									<VisibilityIcon/>
+								</IconButton>
+							</CardActions>
+						</CardContent>
+					</Card>
+				))}
+			</Grid>
+		);
+};
+
+export default RequestCardItem;
--- frontend/src/containers/RequestList/RequestList.js
+++ frontend/src/containers/RequestList/RequestList.js
@@ -13,61 +13,73 @@ import TableContainer from "@material-ui/core/TableContainer";
 import RequestListItem from "./RequestListItem";
 import {getRequests} from "../../store/actions/requestsActions";
 import {wordList} from "../../wordList";
+import Grid from "@material-ui/core/Grid";
+import RequestCardItem from "./RequestCardItem";
 
 const RequestList = () => {
-    const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-    const requests = useSelector(state => state.requests.list);
-    const user = useSelector(state => state.users.user);
-    const language = useSelector(state => state.language.name);
+	const requests = useSelector(state => state.requests.list);
+	const user = useSelector(state => state.users.user);
+	const language = useSelector(state => state.language.name);
 
-    useEffect(() => {
-        dispatch(getRequests());
-    }, [dispatch]);
+	useEffect(() => {
+		dispatch(getRequests());
+	}, [dispatch]);
 
-    let requestsList = requests.map((elem) => (
-        <RequestListItem
-            key={elem._id}
-            id={elem._id}
-            user={elem.user}
-            status={elem.status}
-            userRoleInfo={user.role}
-            date={elem.date}
-        />
-    ));
+	let requestsList = requests.map((elem) => (
+		<RequestListItem
+			key={elem._id}
+			id={elem._id}
+			user={elem.user}
+			status={elem.status}
+			userRoleInfo={user.role}
+			date={elem.date}
+		/>
+	));
 
-    if (user && user.role === 'courier') {
-        requestsList = requests.map(elem => (
-            <RequestListItem
-                key={elem.request._id}
-                id={elem.request._id}
-                status={elem.status}
-                user={elem.request.user}
-                date={elem.request.date}
-            />
-        ))
-    }
+	if (user && user.role === 'courier') {
+		requestsList = requests.map(elem => (
+			<RequestListItem
+				key={elem.request._id}
+				id={elem.request._id}
+				status={elem.status}
+				user={elem.request.user}
+				date={elem.request.date}
+			/>
+		))
+	}
 
-    return (
-        <div>
-            <TableContainer component={Paper}>
-                <Table aria-label="caption table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell> <b>{wordList[language].requestList.tableUser}</b> </TableCell>
-                            <TableCell> <b>{wordList[language].requestList.tableDate}</b> </TableCell>
-                            <TableCell> <b>{wordList[language].requestList.tableStatus}</b> </TableCell>
-                            <TableCell> </TableCell>
-                            <TableCell> </TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {requestsList}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </div>
-    );
+	return (
+		<>
+			{window.innerWidth >= 1200 &&
+			<Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+				<TableContainer component={Paper}>
+					<Table aria-label="caption table">
+						<TableHead>
+							<TableRow>
+								<TableCell> <b>{wordList[language].requestList.tableUser}</b> </TableCell>
+								<TableCell> <b>{wordList[language].requestList.tableDate}</b> </TableCell>
+								<TableCell> <b>{wordList[language].requestList.tableStatus}</b> </TableCell>
+								<TableCell> </TableCell>
+								<TableCell> </TableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{requestsList}
+						</TableBody>
+					</Table>
+				</TableContainer>
+			</Grid>
+			}
+
+			{window.innerWidth <= 1200 &&
+			<>
+                <RequestCardItem/>
+			</>
+			}
+		</>
+	);
 };
 
 export default RequestList;
