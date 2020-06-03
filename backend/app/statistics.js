3073cd3615a45689f96a650d5aba7d34f8212984
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-06-03 15:04:06 +0600

    Переделал получения статистики продукта без использования модели statistics.js #139

--- backend/app/statistics.js
+++ backend/app/statistics.js
@@ -2,21 +2,31 @@ const express = require('express');
 
 const Statistics = require('../models/Statistic'),
     Product = require('../models/Product'),
-    User = require('../models/User');
+    User = require('../models/User'),
+    Request = require('../models/Request');
 const isAuth = require('../middleware/isAuth');
 
 const router = express.Router();
 
 router.get('/product/:id/:date', isAuth, async (req, res) => {
     try {
-        const statistic = await Statistics.find({
-            product: req.params.id,
-            date: {$gte: new Date(new Date() - req.params.date * 60 * 60 * 24 * 1000)}
-        }).populate(['user', 'product']);
+        const id = req.params.id;
+
+        const data = await Request.find({
+            products: {$elemMatch: { product: id }},
+            date: {$gte: new Date(new Date() - req.params.date * 60 * 60 * 24 * 1000)},
+            status: 'closed'
+        }).select({ products: 1, date: 1 });
+
+        const statistic = data.map((elem, index) => {
+            const product = elem.products.filter(product => product.product.toString() === id.toString() )[0];
+            const date = data[index].date;
+            return { product, date }
+        }).flat();
 
         const product = await Product.findOne({_id: req.params.id});
 
-        res.send({product, statistic})
+        res.send({statistic, product})
     } catch (e) {
         res.status(500).send(e)
     }
