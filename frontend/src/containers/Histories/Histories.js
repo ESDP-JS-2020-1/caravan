9fa84214f93094cb62915722ce743111c3db2ea6
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-06-04 15:32:51 +0600

    Реализовал отображение истории на мобильных устройствах #142

--- frontend/src/containers/Histories/Histories.js
+++ frontend/src/containers/Histories/Histories.js
@@ -1,7 +1,6 @@
 import React, {useEffect} from 'react';
 
 import {useDispatch, useSelector} from "react-redux";
-import moment from "moment";
 
 import {makeStyles} from "@material-ui/core/styles";
 import TableContainer from "@material-ui/core/TableContainer";
@@ -11,14 +10,15 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 import TableBody from "@material-ui/core/TableBody";
-import Chip from "@material-ui/core/Chip";
 
 import {getHistoriesList} from "../../store/actions/HistoriesActions";
 
 import {wordList} from "../../wordList";
-import {NavLink} from "react-router-dom";
 import Typography from "@material-ui/core/Typography";
 import Pagination from "@material-ui/lab/Pagination";
+import Hidden from "@material-ui/core/Hidden";
+import HistoriesListItem from "./HistoriesListItem/HistoriesListItem";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles({
     table: {
@@ -32,11 +32,30 @@ const useStyles = makeStyles({
     }
 });
 
+
 const Histories = props => {
     const classes = useStyles();
     const dispatch = useDispatch();
     const histories = useSelector(state => state.histories.historiesList);
     const language = useSelector(state => state.language.name);
+
+    const determineType = (type) => {
+        switch (type) {
+            case 'edit':
+                return 'редактировал';
+            case 'delete':
+                return 'удалил';
+            case 'add':
+                return 'добавил';
+            default:
+                return 'Not found';
+        }
+    }
+    const determineInfo = (info) => {
+        if (info.displayName) return  info.displayName;
+        if (info.name) return  info.name;
+        if (info.status) return  'заявку';
+    }
 
     useEffect(() => {
         dispatch(getHistoriesList(props.match.params.page, props.match.params.limit));
@@ -50,93 +69,65 @@ const Histories = props => {
                 onChange={(e, num) => props.history.push(`/history/ ${num} / 10 `)}
             />
             {histories.length !== 0 ?
-                <TableContainer component={Paper}>
-                    <Table className={classes.table} aria-label="simple table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>{wordList[language].histories.tableInfo}</TableCell>
-                                <TableCell align="right">{wordList[language].histories.tableType}</TableCell>
-                                <TableCell align="right">{wordList[language].histories.tableDate}</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {histories.history.docs && histories.history.docs.map((history) => {
+                <>
+                    <Hidden smDown>
+                        <TableContainer component={Paper}>
+                            <Table className={classes.table} aria-label="simple table">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>{wordList[language].histories.tableInfo}</TableCell>
+                                        <TableCell align="right">{wordList[language].histories.tableType}</TableCell>
+                                        <TableCell align="right">{wordList[language].histories.tableDate}</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {histories.history.docs && histories.history.docs.map((history) => {
+                                        const info = history.info.data;
+                                        const schemaNameInPlural = history.info.schemaNameInPlural;
+
+                                        const userName = history.user.displayName;
+                                        let operationType = determineType(history.type);
+                                        let documentInfo = determineInfo(info);
+
+                                        return (
+                                            <HistoriesListItem
+                                                key={history._id}
+                                                userName={userName}
+                                                operationType={operationType}
+                                                documentInfo={documentInfo}
+                                                info={info}
+                                                schemaNameInPlural={schemaNameInPlural}
+                                                history={history}
+                                            />
+                                        )
+                                    })}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </Hidden>
+                    <Hidden mdUp>
+                        <Grid container spacing={1}>
+                            {histories.history.docs.map((history) => {
                                 const info = history.info.data;
                                 const schemaNameInPlural = history.info.schemaNameInPlural;
 
                                 const userName = history.user.displayName;
-                                let operationType = '';
-                                let documentInfo = '';
+                                let operationType = determineType(history.type);
+                                let documentInfo = determineInfo(info);
 
-                                if (userName !== 'fixtureOperation') {
-                                    switch (history.type) {
-                                        case 'edit':
-                                            operationType = 'редактировал';
-                                            break;
-                                        case 'delete':
-                                            operationType = 'удалил';
-                                            break;
-                                        case 'add':
-                                            operationType = 'добавил';
-                                            break;
-                                        default:
-                                            operationType = 'Not found';
-                                            break;
-                                    }
-                                }
-
-                                if (info.displayName) {
-                                    documentInfo = info.displayName;
-                                    operationType += ' пользователя'
-                                }
-                                if (info.name) documentInfo = info.name;
-                                if (info.status) documentInfo = 'заявку';
-
-                                return (
-                                    <TableRow key={history._id}>
-                                        <TableCell component="th" scope="row">
-                                            <div className={classes.title}>
-                                                <b>{userName}</b>
-                                                <p>{operationType}</p>
-                                                {history.type === 'delete' ?
-                                                    <p>{documentInfo}</p> :
-                                                    <NavLink exact
-                                                             to={info.productType ?
-                                                                 `/?product=${info.name}`
-                                                                :`${schemaNameInPlural}/${info._id}`
-                                                             }>{documentInfo}</NavLink>}
-                                            </div>
-                                        </TableCell>
-                                        <TableCell align="right">
-                                            {history.type === 'delete' && (
-                                                <Chip
-                                                    size="small"
-                                                    label={wordList[language].histories.deleteLabel}
-                                                    color="secondary"
-                                                />
-                                            )}
-                                            {history.type === 'add' && (
-                                                <Chip
-                                                    size="small"
-                                                    label={wordList[language].histories.addLabel}
-                                                    color="primary"
-                                                />
-                                            )}
-                                            {history.type === 'edit' && (
-                                                <Chip
-                                                    size="small"
-                                                    label={wordList[language].histories.editLabel}
-                                                />
-                                            )}
-                                        </TableCell>
-                                        <TableCell
-                                            align="right">{moment(history.date).format('MMMM Do YYYY, h:mm:ss a')}</TableCell>
-                                    </TableRow>
-                                )
+                                return <HistoriesListItem
+                                    key={history._id}
+                                    userName={userName}
+                                    operationType={operationType}
+                                    documentInfo={documentInfo}
+                                    info={info}
+                                    schemaNameInPlural={schemaNameInPlural}
+                                    history={history}
+                                />
                             })}
-                        </TableBody>
-                    </Table>
-                </TableContainer> :
+                        </Grid>
+                    </Hidden>
+                </> :
                 <Typography variant='h3'>
                     {'В истории пока ничего нет!'}
                 </Typography>}
