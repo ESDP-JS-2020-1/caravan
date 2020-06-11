3373ef7894a461621ace69e1ed1640cdca58255f
ramazan <assasins11122001@gmail.com>
2020-06-11 12:31:47 +0600

    Сделал action creators для статистики пользователей #149

--- frontend/src/store/actions/statisticsActions.js
+++ frontend/src/store/actions/statisticsActions.js
@@ -1,32 +1,32 @@
-import {GET_STATISTIC_ERROR, GET_STATISTIC_REQUEST, GET_STATISTIC_SUCCESS} from "./actionsTypes";
+import {GET_PRODUCT_STATISTIC_SUCCESS, GET_PRODUCT_STATISTIC_ERROR, GET_USER_STATISTIC_SUCCESS} from "./actionsTypes";
 import axiosApi from "../../axiosAPI";
 
 import {push} from 'connected-react-router';
 
-export const getStatisticRequest = () => ({type: GET_STATISTIC_REQUEST});
-export const getStatisticSuccess = (statistics) => ({type: GET_STATISTIC_SUCCESS, statistics});
-export const getStatisticError = (error) => ({type: GET_STATISTIC_ERROR, error});
+export const getProductStatisticSuccess = productStatistic => ({type: GET_PRODUCT_STATISTIC_SUCCESS, productStatistic});
+export const getProductStatisticError = error => ({type: GET_PRODUCT_STATISTIC_ERROR, error});
+
+export const getUserStatisticSuccess = userStatistic => ({type: GET_USER_STATISTIC_SUCCESS, userStatistic});
+export const getUserStatisticError = error => ({type: GET_USER_STATISTIC_SUCCESS, error});
 
 export const getStatisticsProduct = (productId, day) => async dispatch => {
     try {
-        dispatch(getStatisticRequest());
         const response = await axiosApi.get(`/stat/product/${productId}/${day}`);
-        dispatch(getStatisticSuccess(response.data));
+        dispatch(getProductStatisticSuccess(response.data));
 
         dispatch(push(`/product/stat/${productId}/${day}`))
     } catch (e) {
-        dispatch(getStatisticError(e))
+        dispatch(getProductStatisticError(e))
     }
 };
 
 export const getStatisticsUser = (userId, day) => async dispatch => {
     try {
-        dispatch(getStatisticRequest());
         const response = await axiosApi.get(`/stat/user/${userId}/${day}`);
-        dispatch(getStatisticSuccess(response.data));
+        dispatch(getUserStatisticSuccess(response.data));
 
         dispatch(push(`/users/stat/${userId}/${day}`))
     } catch (e) {
-        dispatch(getStatisticError(e))
+        dispatch(getUserStatisticError(e))
     }
 };
