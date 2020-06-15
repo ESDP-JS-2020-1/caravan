6438f9eaeedd0e3a4f65815ba880dbe950b367e9
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-06-15 13:12:43 +0600

    Убрал приставку _PERMISSION в списке пермишенов

--- backend/permissions.js
+++ backend/permissions.js
@@ -1,26 +1,26 @@
 module.exports = {
-    GET_USER_PERMISSION: 'getUser',
-    ADD_USER_PERMISSION: 'addUser',
-    DELETE_USER_PERMISSION: 'deleteUser',
-    EDIT_USER_PERMISSION: 'editUser',
+    GET_USER: 'getUser',
+    ADD_USER: 'addUser',
+    DELETE_USER: 'deleteUser',
+    EDIT_USER: 'editUser',
 
-    ADD_PRODUCT_PERMISSION: 'addProduct',
-    DELETE_PRODUCT_PERMISSION: 'deleteProduct',
-    EDIT_PRODUCT_PERMISSION: 'editProduct',
+    ADD_PRODUCT: 'addProduct',
+    DELETE_PRODUCT: 'deleteProduct',
+    EDIT_PRODUCT: 'editProduct',
 
-    GET_GROUP_PERMISSION: 'getGroup',
-    EDIT_GROUP_PERMISSION: 'editGroup',
-    ADD_GROUP_PERMISSION: 'addGroup',
-    DELETE_GROUP_PERMISSION: 'deleteGroup',
+    GET_GROUP: 'getGroup',
+    EDIT_GROUP: 'editGroup',
+    ADD_GROUP: 'addGroup',
+    DELETE_GROUP: 'deleteGroup',
 
-    GET_REQUEST_PERMISSION: 'getRequest',
-    ADD_REQUEST_PERMISSION: 'addRequest',
-    DELETE_REQUEST_PERMISSION: 'deleteRequest',
-    EDIT_REQUEST_PERMISSION: 'editRequest',
-    CLOSE_REQUEST_PERMISSION: 'closeRequest',
+    GET_REQUEST: 'getRequest',
+    ADD_REQUEST: 'addRequest',
+    DELETE_REQUEST: 'deleteRequest',
+    EDIT_REQUEST: 'editRequest',
+    CLOSE_REQUEST: 'closeRequest',
 
-    VIEW_HISTORY_PERMISSION: 'viewHistory',
-    GET_STATISTIC_PERMISSION: 'getStatistic',
+    VIEW_HISTORY: 'viewHistory',
+    GET_STATISTIC: 'getStatistic',
 
     GET_TRASH: 'getTrash'
 };
