5e47a2b30b80acd6d5737dc80b21e4af469677ad
Ulan Beltaev <ulanbeltaev@gmail.com>
2020-06-22 14:26:44 +0600

    Добавил перевод на кыргызский язык

--- frontend/src/containers/ProductList/MainPageMap/MainPageMap.js
+++ frontend/src/containers/ProductList/MainPageMap/MainPageMap.js
@@ -6,8 +6,10 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import {makeStyles} from "@material-ui/core/styles";
+import {useSelector} from "react-redux";
+import {wordList} from "../../../wordList";
 
-const {BaseLayer} = LayersControl
+const {BaseLayer} = LayersControl;
 
 const useStyles = makeStyles({
     map: {
@@ -18,7 +20,7 @@ const useStyles = makeStyles({
         display: "flex",
         flexDirection: 'column'
     }
-})
+});
 
 const MainPageMap = (
     {users}
@@ -28,19 +30,22 @@ const MainPageMap = (
     const [mapConfig] = useState({
         center: [42.8746, 74.5698],
         zoom: 12
-    })
+    });
+
+    const language = useSelector(state => state.language.name);
 
     const layers = {
         osm: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
         hot: "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
         dark: "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png",
         cycle: "https://dev.{s}.tile.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png"
-    }
+    };
+
     return (
         <Grid item container direction='column'>
             <Grid item>
                 <Typography variant='h3' paragraph>
-                    Карта торговых точек
+                    {wordList[language].mainPageMap.title}
                 </Typography>
             </Grid>
             <Grid item xs>
@@ -62,8 +67,8 @@ const MainPageMap = (
                             {users && users.map(user => {
                                 if (user.role === 'market') {
                                     const userData = user.market;
-                                    const lat = userData.coordinates.lat
-                                    const lng = userData.coordinates.lng
+                                    const lat = userData.coordinates.lat;
+                                    const lng = userData.coordinates.lng;
                                     return <Marker position={[lat, lng]} key={user._id}>
                                         <Popup>
                                             <div className={classes.popup}>
