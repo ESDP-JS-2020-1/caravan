2ee6326549214fc32d333fb8bdb1a6399362575c
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-06-29 14:54:47 +0600

    Исправил верстку у компонента footer #181

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -47,7 +47,7 @@ const useStyles = makeStyles((theme) => ({
         marginBottom: 'auto'
     },
     footer: {
-        height: '150px',
+        maxHeight: '150px',
         padding: theme.spacing(3, 2),
         marginTop: 'auto',
         backgroundColor: '#0d47a1'
